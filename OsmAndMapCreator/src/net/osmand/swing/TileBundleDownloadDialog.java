e38885576caa69362e1a0c9ace9f5690bb5c5003
Victor Shcherb <osmand.app@gmail.com>
2013-01-24 23:22:56 +0100

    Restructure packages

--- OsmAndMapCreator/src/net/osmand/swing/TileBundleDownloadDialog.java
+++ OsmAndMapCreator/src/net/osmand/swing/TileBundleDownloadDialog.java
@@ -25,10 +25,10 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import net.osmand.MapCreatorVersion;
-import net.osmand.data.MapTileDownloader;
-import net.osmand.data.MapTileDownloader.DownloadRequest;
-import net.osmand.data.MapTileDownloader.IMapDownloaderCallback;
 import net.osmand.map.ITileSource;
+import net.osmand.map.MapTileDownloader;
+import net.osmand.map.MapTileDownloader.DownloadRequest;
+import net.osmand.map.MapTileDownloader.IMapDownloaderCallback;
 import net.osmand.osm.MapUtils;
 import net.osmand.swing.MapPanel.MapSelectionArea;
 
