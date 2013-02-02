be3a6a5c4d0ad33c774c1afcdb41573fe863583c
Victor Shcherb <osmand.app@gmail.com>
2013-02-02 16:56:04 +0100

    add ignore

--- OsmAndMapCreator/src-tests/net/osmand/router/TestRoutingWithNative.java
+++ OsmAndMapCreator/src-tests/net/osmand/router/TestRoutingWithNative.java
@@ -1,7 +1,10 @@
 package net.osmand.router;
 
+import org.junit.Ignore;
+
 import net.osmand.swing.NativeSwingRendering;
 
+@Ignore
 public class TestRoutingWithNative {
 	
 	public static void main(String[] args) throws Exception {
