f71cdbca582708760080176f015e35a207bbdda2
Victor Shcherb <victor.shcherb@gmail.com>
2013-02-21 00:57:17 +0100

    add build params

--- OsmAndMapCreator/native-build.sh
+++ OsmAndMapCreator/native-build.sh
@@ -9,11 +9,11 @@ core/externals/configure.sh
 if [ ! -d baked/amd64-linux-gcc-amd64-linux-gcc ]; then 
 build/amd64-linux-gcc.sh
 fi
-cd baked/amd64-linux-gcc-amd64-linux-gcc && make
+(cd baked/amd64-linux-gcc-amd64-linux-gcc && make)
 cp binaries/linux/amd64/libOsmAndCore.so "$LIB_LOC"/OsmAndCore-linux-amd64.lib
 
 if [ ! -d baked/i686-linux-gcc-i686-linux-gcc ]; then 
 build/i686-linux-gcc.sh 
 fi
-cd baked/i686-linux-gcc-i686-linux-gcc && make
+(cd baked/i686-linux-gcc-i686-linux-gcc && make)
 cp binaries/linux/i686/libOsmAndCore.so "$LIB_LOC"/OsmAndCore-linux-x86.lib
