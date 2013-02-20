f0c88fb2e8ea09c41373c82b0a175ef255e82ef2
Victor Shcherb <victor.shcherb@gmail.com>
2013-02-21 00:49:55 +0100

    Linux build

---
+++ OsmAndMapCreator/native-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SCRIPT_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LIB_LOC="$SCRIPT_LOC"/src/net/osmand/swing/
+cd "$SCRIPT_LOC"/../../../
+
+core/externals/configure.sh
+
+if [! -d baked/amd64-linux-gcc-amd64-linux-gcc ]; then 
+build/amd64-linux-gcc.sh
+fi
+cd baked/amd64-linux-gcc-amd64-linux-gcc && make
+cp binaries/linux/amd64/libOsmAndCore.so "$LIB_LOC"/OsmAndCore-linux-amd64.lib
+
+if [! -d baked/i686-linux-gcc-i686-linux-gcc ]; then 
+build/i686-linux-gcc.sh 
+fi
+cd baked/i686-linux-gcc-i686-linux-gcc && make
+cp binaries/linux/i686/libOsmAndCore.so "$LIB_LOC"/OsmAndCore-linux-x86.lib
