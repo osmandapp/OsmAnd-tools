ddbea0b427d4b5e111df4d0765300c1ce6b9b9ee
Victor Shcherb <victor.shcherb@gmail.com>
2013-02-21 01:08:55 +0100

    Fix native build

--- OsmAndMapCreator/native-build.sh
+++ OsmAndMapCreator/native-build.sh
@@ -5,6 +5,7 @@ LIB_LOC="$SCRIPT_LOC"/src/net/osmand/swing/
 cd "$SCRIPT_LOC"/../../
 
 core/externals/configure.sh
+core/qtbase-desktop/build.sh # neeeded?
 
 if [ ! -d baked/amd64-linux-gcc-amd64-linux-gcc ]; then 
 build/amd64-linux-gcc.sh
@@ -12,8 +13,8 @@ fi
 (cd baked/amd64-linux-gcc-amd64-linux-gcc && make)
 cp binaries/linux/amd64/libOsmAndCore.so "$LIB_LOC"/OsmAndCore-linux-amd64.lib
 
-if [ ! -d baked/i686-linux-gcc-i686-linux-gcc ]; then 
-build/i686-linux-gcc.sh 
-fi
-(cd baked/i686-linux-gcc-i686-linux-gcc && make)
-cp binaries/linux/i686/libOsmAndCore.so "$LIB_LOC"/OsmAndCore-linux-x86.lib
+# if [ ! -d baked/i686-linux-gcc-i686-linux-gcc ]; then 
+# build/i686-linux-gcc.sh 
+# fi
+# (cd baked/i686-linux-gcc-i686-linux-gcc && make)
+# cp binaries/linux/i686/libOsmAndCore.so "$LIB_LOC"/OsmAndCore-linux-x86.lib
