3003515f1ea9a1b68f4810d8369a506af4b46e79
Nelson A. de Oliveira <naoliv@debian.org>
2013-07-31 11:19:24 -0300

    Replace StringBuffer by StringBuilder
    java.lang.StringBuffer may be more efficiently declared as
    java.lang.StringBuilder. java.lang.StringBuilder is a non-thread-safe
    replacement for java.lang.StringBuffer, available in Java 5 and newer

--- OsmAndMapCreator/src/net/osmand/data/index/IndexUploader.java
+++ OsmAndMapCreator/src/net/osmand/data/index/IndexUploader.java
@@ -880,7 +880,7 @@ public class IndexUploader {
 			return b;
 
 		if (b == 1 || b == 2) {
-			StringBuffer sb = new StringBuffer();
+			StringBuilder sb = new StringBuilder();
 			int c;
 			do {
 				c = in.read();
