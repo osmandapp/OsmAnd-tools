2f56d4700e95e3e4614def8cfca231894d58341a
Nelson A. de Oliveira <naoliv@debian.org>
2013-07-31 10:46:33 -0300

    Remove redundant cast expressions

--- OsmAndMapCreator/src/net/osmand/data/preparation/IndexVectorMapCreator.java
+++ OsmAndMapCreator/src/net/osmand/data/preparation/IndexVectorMapCreator.java
@@ -571,7 +571,7 @@ public class IndexVectorMapCreator extends AbstractIndexPartCreator {
 		long baseId = 0;
 		for (int i = 0; i < parent.getTotalElements(); i++) {
 			if (e[i].getElementType() == rtree.Node.LEAF_NODE) {
-				long id = ((LeafElement) e[i]).getPtr();
+				long id = e[i].getPtr();
 				selectData.setLong(1, id);
 				// selectData = mapConnection.prepareStatement("SELECT area, coordinates, innerPolygons, types, additionalTypes, name FROM binary_map_objects WHERE id = ?");
 				ResultSet rs = selectData.executeQuery();
@@ -617,7 +617,7 @@ public class IndexVectorMapCreator extends AbstractIndexPartCreator {
 		}
 		for (int i = 0; i < parent.getTotalElements(); i++) {
 			if (e[i].getElementType() != rtree.Node.LEAF_NODE) {
-				long ptr = ((NonLeafElement) e[i]).getPtr();
+				long ptr = e[i].getPtr();
 				rtree.Node ns = r.getReadNode(ptr);
 				writeBinaryMapBlock(ns, e[i].getRect(), r, writer, selectData, bounds, tempStringTable, tempNames,level);
 			}
@@ -639,7 +639,7 @@ public class IndexVectorMapCreator extends AbstractIndexPartCreator {
 		}
 		for (int i = 0; i < parent.getTotalElements(); i++) {
 			if (e[i].getElementType() != rtree.Node.LEAF_NODE) {
-				rtree.Node chNode = r.getReadNode(((NonLeafElement) e[i]).getPtr());
+				rtree.Node chNode = r.getReadNode(e[i].getPtr());
 				writeBinaryMapTree(chNode, e[i].getRect(), r, writer, bounds);
 			}
 		}
