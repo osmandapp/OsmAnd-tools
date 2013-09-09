bffbb708260d22743427b5b795b8716dd67b22b2
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-09-09 08:21:53 +0300

    Update external sources to download fresh versions, keep obsolete

--- mirror_externals.sh
+++ mirror_externals.sh
@@ -1,4 +1,11 @@
 curl -L http://sourceforge.net/projects/expat/files/expat/2.1.0/expat-2.1.0.tar.gz/download > expat-2.1.0.tar.gz
+curl -L http://sourceforge.net/projects/freetype/files/freetype2/2.5.0/freetype-2.5.0.1.tar.bz2/download > freetype-2.5.0.1.tar.bz2
+curl -L http://sourceforge.net/projects/giflib/files/giflib-4.x/giflib-4.2.3.tar.bz2/download > giflib-4.2.3.tar.bz2
+curl -L http://www.ijg.org/files/jpegsrc.v9.tar.gz > jpegsrc.v9.tar.gz
+curl -L http://sourceforge.net/projects/libpng/files/libpng16/1.6.3/libpng-1.6.3.tar.xz/download > libpng-1.6.3.tar.xz
+curl -L http://sourceforge.net/projects/libpng/files/zlib/1.2.8/zlib-1.2.8.tar.gz/download > zlib-1.2.8.tar.bz2
+
+# Obsolete
 curl -L http://sourceforge.net/projects/freetype/files/freetype2/2.4.10/freetype-2.4.10.tar.bz2/download > freetype-2.4.10.tar.bz2
 curl -L http://sourceforge.net/projects/giflib/files/giflib-4.x/giflib-4.2.1.tar.bz2/download > giflib-4.2.1.tar.bz2
 curl -L http://www.ijg.org/files/jpegsrc.v8d.tar.gz > jpegsrc.v8d.tar.gz
