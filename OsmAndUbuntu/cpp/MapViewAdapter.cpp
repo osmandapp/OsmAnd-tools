f6516f6f22ddd7209f958f3cb70b942c67a61b48
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-08-21 19:10:09 +0300

    Cleanup

--- OsmAndUbuntu/cpp/MapViewAdapter.cpp
+++
@@ -1,7 +0,0 @@
-#include "MapViewAdapter.h"
-
-MapViewAdapter::MapViewAdapter(QObject *) : app(OsmAnd::OsmAndApplication::getAndInitializeApplication())
-{
-
-    mapView = std::shared_ptr<OsmAnd::OsmAndMapView>(new OsmAnd::OsmAndMapView(app));
-}
