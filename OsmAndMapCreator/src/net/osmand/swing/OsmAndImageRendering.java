25209b93c7c827baa310a015f28906768b099f93
vshcherb <victor.shcherb@gmail.com>
2014-09-02 11:32:55 +0200

    Update OsmAndImageRendering.java

--- OsmAndMapCreator/src/net/osmand/swing/OsmAndImageRendering.java
+++ OsmAndMapCreator/src/net/osmand/swing/OsmAndImageRendering.java
@@ -230,14 +230,14 @@ public class OsmAndImageRendering {
 						String line = eyepiece;
 						double dx = ic.zoomScale + 1;
 						line += " -latLon=" + lat+";"+lon;
-						line += " -stylesPath="+stylesPath;
+						line += " -stylesPath=\""+stylesPath+"\"";
 						String styleName = ic.renderingStyle;
 						if(styleName.endsWith(".render.xml")) {
 							styleName = styleName.substring(0, styleName.length() - ".render.xml".length());
 						}
 						line += " -styleName="+styleName;
 						for(File f : obfFiles) {
-							line += " -obfFile="+f.getAbsolutePath();
+							line += " -obfFile=\""+f.getAbsolutePath()+"\"";
 						}
 						String[] listProps = ic.renderingProperties.split(",");
 						for(String p : listProps) {
@@ -249,7 +249,7 @@ public class OsmAndImageRendering {
 						line += " -outputImageWidth=" + imageWidth;
 						line += " -outputImageHeight=" + imageHeight;
 						final String fileName = ic.generateName + ".png";
-						line += " -outputImageFilename=" + outputFiles + "/" + fileName;
+						line += " -outputImageFilename=\"" + outputFiles + "/" + fileName+"\"";
 						line += " -referenceTileSize="+(int)(dx*256);
 						line += " -displayDensityFactor="+dx;
 						line += " -zoom="+ic.zoom;
