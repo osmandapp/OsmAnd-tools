ab772a379a9d1f76dc70f0521e62bedd9199d242
vshcherb <victor.shcherb@gmail.com>
2015-01-07 15:33:49 +0100

    Update query_polygons_osm.py

--- obf-generation/basemap/query_polygons_osm.py
+++ obf-generation/basemap/query_polygons_osm.py
@@ -139,8 +139,8 @@ def process_polygons(tags, filename):
 	f.write('</osm>')
 
 if __name__ == "__main__":
-		process_polygons(['lake'], 'polygon_lake_water.osm')
-		process_polygons(['landuse', 'natural', 'historic','leisure'], 'polygon_natural_landuse.osm')
-		process_polygons(['aeroway', 'military', 'power', 'tourism'], 'polygon_aeroway_military_tourism.osm')
+		#process_polygons(['lake'], 'polygon_lake_water.osm')
+		#process_polygons(['landuse', 'natural', 'historic','leisure'], 'polygon_natural_landuse.osm')
+		#process_polygons(['aeroway', 'military', 'power', 'tourism'], 'polygon_aeroway_military_tourism.osm')
 		#-1175256, -1751158 causing troubles 
 		process_polygons(['admin_level'], 'polygon_admin_level.osm') 
