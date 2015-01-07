30e7ea2fbe8431d33221b836c5b013bc9168a2db
vshcherb <victor.shcherb@gmail.com>
2015-01-07 15:33:36 +0100

    Update query_roads_osm.py

--- obf-generation/basemap/query_roads_osm.py
+++ obf-generation/basemap/query_roads_osm.py
@@ -78,13 +78,14 @@ def process_roads(cond, filename, fields):
 	f.write('</osm>')
 
 if __name__ == "__main__":
-	process_roads("highway='motorway'", "line_motorway.osm", ['highway', 'junction', 'route'])
-	process_roads("highway='trunk'", "line_trunk.osm", ['highway', 'junction', 'route'])
-	process_roads("highway='primary'", "line_primary.osm", ['highway', 'junction', 'route'])
-	process_roads("highway='secondary'", "line_secondary.osm", ['highway', 'junction', 'route'])
-	process_roads("railway='rail'", "line_railway.osm", ['railway'])
-	process_roads("highway='tertiary'", "line_tertiary.osm", ['highway', 'junction', 'route'])
-	process_roads("route='ferry' or (tags->'seamark:type' in ('separation_line', 'separation_lane', 'separation_boundary', 'light_major'))", "line_ferry.osm", ['route', 'seamark:type'])
+	#process_roads("highway='motorway'", "line_motorway.osm", ['highway', 'junction', 'route'])
+	#process_roads("highway='trunk'", "line_trunk.osm", ['highway', 'junction', 'route'])
+	#process_roads("highway='primary'", "line_primary.osm", ['highway', 'junction', 'route'])
+	#process_roads("highway='secondary'", "line_secondary.osm", ['highway', 'junction', 'route'])
+	#process_roads("railway='rail'", "line_railway.osm", ['railway'])
+	#process_roads("highway='tertiary'", "line_tertiary.osm", ['highway', 'junction', 'route'])
+	#process_roads("route='ferry'", "line_ferry.osm", ['route'])
+	process_roads("(tags->'seamark:type' in ('separation_line', 'separation_lane', 'separation_boundary', 'light_major'))", "proc_line_seamark.osm", [ 'seamark:type'])
 	
 	# not used
 	#process_roads("(admin_level = '4' or admin_level = '2')", "line_admin_level.osm", ['admin_level'])
