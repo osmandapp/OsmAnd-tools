46feb32a1c68d3587135bcd44e406d55c04c0369
xmd5a <lornaderek@mail.ru>
2015-06-16 20:14:35 +0300

    More languages

--- OsmAndMapCreator/src/net/osmand/osm/MapRenderingTypesEncoder.java
+++ OsmAndMapCreator/src/net/osmand/osm/MapRenderingTypesEncoder.java
@@ -30,8 +30,8 @@ public class MapRenderingTypesEncoder extends MapRenderingTypes {
 	private Map<String, List<EntityConvert>> convertTags = new HashMap<String, List<EntityConvert>>();
 	private MapRulType coastlineRuleType;
 	
-	private String[] langs = new String[] { "ar", "be", "ca", "cs", "da", "de", "el", "es", "fi", "fr", "he", "hi",
-			"hr", "hu", "it", "ja", "ko", "lt", "lv", "nl", "pl", "pt", "ro", "ru", "sk", "sl", "sr", "sv", "sw", "zh" };
+	private String[] langs = new String[] { "af", "ar", "az", "be", "bg", "bn", "bpy", "br", "bs", "ca", "ceb", "cs", "cy", "da", "de", "el", "eo", "es", "et", "eu", "id", "fa", "fi", "fr", "fy", "ga", "gl", "he", "hi",
+			"hr", "ht", "hu", "hy", "is", "it", "ja", "ka", "ko", "ku", "la", "lb", "lt", "lv", "mk", "ml", "mr", "ms", "nds", "new", "nl", "nn", "no", "nv", "os", "pl", "pms", "pt", "ro", "ru", "sc", "sh", "sk", "sl", "sq", "sr", "sv", "sw", "ta", "te", "th", "tl", "tr", "uk", "vi", "vo", "zh" };
 	
 	public MapRenderingTypesEncoder(String fileName) {
 		super(fileName);
