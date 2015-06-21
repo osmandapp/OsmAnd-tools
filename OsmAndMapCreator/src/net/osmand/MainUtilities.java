fc8091fa6b9fd6e349a3ed6d46f68e668728bf64
Victor Shcherb <victor.shcherb@gmail.com>
2015-06-21 23:59:22 +0200

    Add more utilities

--- OsmAndMapCreator/src/net/osmand/MainUtilities.java
+++ OsmAndMapCreator/src/net/osmand/MainUtilities.java
@@ -17,6 +17,7 @@ import net.osmand.impl.ConsoleProgressImplementation;
 import net.osmand.osm.MapRenderingTypes;
 import net.osmand.osm.MapRenderingTypesEncoder;
 import net.osmand.osm.util.ResourceDeleter;
+import net.osmand.regions.CountryOcbfGeneration;
 
 public class MainUtilities {
 	private static Log log = PlatformUtil.getLog(MainUtilities.class);
@@ -35,6 +36,8 @@ public class MainUtilities {
 				OceanTilesCreator.createTilesFile(subArgsArray[0], subArgsArray.length > 1 ? args[1] : null);
 			} else if(utl.equals("test-routing")) {
 				net.osmand.router.TestRouting.main(subArgsArray);
+			} else if(utl.equals("generate-ocbf")) {
+				CountryOcbfGeneration.main(subArgsArray);
 			} else if(utl.equals("generate-obf")) {
 				IndexCreator ic = new IndexCreator(new File("."));
 				ic.setIndexMap(true);
@@ -80,6 +83,7 @@ public class MainUtilities {
 		System.out.println("\t\t check-ocean-tile <lat> <lon> <zoom=11>: checks ocean or land tile is in bz2 list");
 		System.out.println("\t\t generate-ocean-tile <coastline osm file> <optional output file>: creates ocean tiles 12 zoom");
 		System.out.println("\t\t test-routing <own list of parameters>: helps to run routing test for specific locations");
+		System.out.println("\t\t generate-ocbf <path to osmand/repos/ repository>: generates regions.ocbf file, this path should contain folders 'misc', 'tools', 'resources'");
 		System.out.println("\t\t delete-unused-strings <path to repos/android/OsmAnd/res>: deletes unused translation in git repository (transforms all strings.xml)");
 		System.out.println("\t\t generate-obf <path to osm file>: simple way to generate obf file in place. "
 				+ "\t\t\t	Another supported options generate-map, generate-address, generate-poi, generate-roads (generate obf partially)");
