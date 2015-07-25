f4e67020b12b2e65c19d8b7bf320e86d341170a3
Victor Shcherb <victor.shcherb@gmail.com>
2015-07-25 12:45:26 +0200

    increase interval

--- OsmAndMapCreator/src/net/osmand/data/preparation/IncOsmChangesCreator.java
+++ OsmAndMapCreator/src/net/osmand/data/preparation/IncOsmChangesCreator.java
@@ -51,7 +51,7 @@ import org.apache.commons.logging.LogFactory;
 public class IncOsmChangesCreator {
 	private static final Log log = LogFactory.getLog(IncOsmChangesCreator.class);
 	private static final int OSC_FILES_TO_COMBINE = 300;
-	private static final long INTERVAL_TO_UPDATE_PBF = 1000 * 60 * 60;
+	private static final long INTERVAL_TO_UPDATE_PBF = 1000 * 60 * 60 * 2;
 	private static final long MB = 1024 * 1024;
 	
 	private void process(String location, String repo, String binaryFolder) throws Exception {
