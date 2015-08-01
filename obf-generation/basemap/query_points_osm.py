888d2802812056ce64178d26801d096662c07402
Victor Shcherb <victor.shcherb@gmail.com>
2015-08-02 00:41:50 +0200

    Test

--- obf-generation/basemap/query_points_osm.py
+++ obf-generation/basemap/query_points_osm.py
@@ -85,7 +85,7 @@ def process_points(cond, filename, array):
 	f.write('</osm>')
 
 if __name__ == "__main__":
-	process_points("place in ('sea','ocean','state', 'country') "
+#	process_points("place in ('sea','ocean','state', 'country') "
 				   " or \"natural\" in ('peak', 'cave_entrance', 'rock', 'waterfall', 'cape', 'volcano', 'stream', 'strait', 'reef')"
 				   " or tourism in ('alpine_hut') "
 				   " or tags->'seamark:type' in ('light_major') "
@@ -94,4 +94,4 @@ if __name__ == "__main__":
 				   " or aeroway in ('aerodrome', 'airport')", 'points.osm', 
 				   ['name', 'name:en',
 				    'ref', 'ele', 'place','natural', 'seamark:type', 'abandoned', 'aeroway', 'tourism', 'iata', 'icao', 'faa'])
-	process_points("place in ('city','town') ", 'cities.osm', ['name', 'name:en', 'place', 'capital'])
+#	process_points("place in ('city','town') ", 'cities.osm', ['name', 'name:en', 'place', 'capital'])
