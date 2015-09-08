670b50e60f956171eac20c602c4bea54ad111bf0
vshcherb <victor.shcherb@gmail.com>
2015-09-08 23:23:25 +0200

    Update query_points_osm.py

--- obf-generation/basemap/query_points_osm.py
+++ obf-generation/basemap/query_points_osm.py
@@ -52,10 +52,10 @@ def process_points(cond, filename, array):
 		array.append(nm)
 		queryFields += ", tags->\'" + nm + "\' as \"" + nm + "\""
 		
-	sql = "select ST_AsText(ST_Transform(way,94326)), osm_id, population " + queryFields + \
+	sql = "select ST_AsText(ST_Transform(way,94326)), osm_id,  " + queryFields + \
 	      " from planet_osm_point where " + cond + ";"
 	      # "LIMIT 2"
-	#print sql
+	print sql
 	cursor.execute(sql)
  
 	node_id =-1000
