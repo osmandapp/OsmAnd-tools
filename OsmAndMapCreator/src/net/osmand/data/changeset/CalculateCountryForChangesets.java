46b1fe52f03bd42110df901f4b9da7eb81877a59
Victor Shcherb <victor.shcherb@gmail.com>
2016-01-12 10:04:10 +0100

    Add queries

---
+++ OsmAndMapCreator/src/net/osmand/data/changeset/CalculateCountryForChangesets.java
@@ -0,0 +1,31 @@
+package net.osmand.data.changeset;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CalculateCountryForChangesets {
+	
+	public static void main(String[] args) throws SQLException {
+		if(args[0].equals("calculate_countries")) {
+			calculateCountries();
+		}
+	}
+
+	private static void calculateCountries() throws SQLException {
+		Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/changeset",
+				System.getProperty("DB_USER"), System.getProperty("DB_PWD"));
+		try {
+			Statement stat = conn.createStatement();
+			ResultSet rs = stat.executeQuery("SELECT COUNT(*) FROM changesets");
+			System.out.println(rs.next());
+			System.out.println(rs.getInt(1));
+		} finally {
+			conn.close();
+		}
+		
+	}
+
+}
