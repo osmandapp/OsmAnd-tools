70ef6cd0f9cda8041eafa86bcb3fa9620f97b4d8
Victor Shcherb <victor.shcherb@gmail.com>
2017-05-29 11:20:11 +0200

    Add exception analyzer

--- support-tools/ExceptionAnalyzer/src/main/java/net/osmand/exceptionanalyzer/ExceptionAnalyzerMain.java
+++ support-tools/ExceptionAnalyzer/src/main/java/net/osmand/exceptionanalyzer/ExceptionAnalyzerMain.java
@@ -1,4 +1,4 @@
-package com.test.exceptionanalyzer;
+package net.osmand.exceptionanalyzer;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
