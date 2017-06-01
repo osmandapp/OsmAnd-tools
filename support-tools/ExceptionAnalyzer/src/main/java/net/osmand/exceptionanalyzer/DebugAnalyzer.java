8f14300b033d01166150d347d2c531dd85c90673
PaulStets <stetsenko.pavlo@gmail.com>
2017-06-01 16:30:25 +0300

    New Parsing Strategy

---
+++ support-tools/ExceptionAnalyzer/src/main/java/net/osmand/exceptionanalyzer/DebugAnalyzer.java
@@ -0,0 +1,11 @@
+package net.osmand.exceptionanalyzer;
+
+/**
+ * Created by user on 01.06.17.
+ */
+public class DebugAnalyzer {
+
+    public static void main(String[] args) {
+        ExceptionAnalyzerMain.makeReport();
+    }
+}
