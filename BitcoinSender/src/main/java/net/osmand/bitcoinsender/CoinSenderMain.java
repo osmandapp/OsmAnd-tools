e0ffd6ea681b371d57105345ff7cdd193fa1a90b
Victor Shcherb <victor.shcherb@gmail.com>
2017-08-02 12:56:17 +0300

    Update coin sender main

--- BitcoinSender/src/main/java/net/osmand/bitcoinsender/CoinSenderMain.java
+++ BitcoinSender/src/main/java/net/osmand/bitcoinsender/CoinSenderMain.java
@@ -22,6 +22,7 @@ public class CoinSenderMain {
     private static String pass;
     private static String directory;
 	private static int PART_SIZE = 80;
+	private static double MIN_PAY = 0.0001;
 
     public static void main(String args[]) throws IOException {
 
@@ -29,14 +30,23 @@ public class CoinSenderMain {
             System.out.println("Usage: --prepare | --send");
             return;
         }
-
-        System.out.print("Enter your API key: ");
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter your API key: ");
         guid = in.nextLine();
         System.out.print("Enter your PIN: ");
         pass = in.nextLine();
         System.out.print("Enter full path to JSON file: ");
         directory = in.nextLine();
+        System.out.print("Enter your part size (default 80): ");
+        String ll = in.nextLine();
+        if(ll.trim().length() > 0) {
+        	PART_SIZE = Integer.parseInt(ll);
+        }
+        System.out.print("Enter minimal amount to pay (default 0.0001): ");
+        ll = in.nextLine();
+        if(ll.trim().length() > 0) {
+        	MIN_PAY = Double.parseDouble(ll);
+        }
 
         if (guid.equals("") || pass.equals("")) {
             System.out.println("You forgot to enter Client_ID or Secret. Exiting...");
@@ -73,7 +83,6 @@ public class CoinSenderMain {
         }
         JsonReader reader = new JsonReader(new FileReader(directory));
         recipients.putAll((Map) gson.fromJson(reader, Map.class));
-        System.out.println(recipients);
         List<LinkedTreeMap> paymentsList = (ArrayList) recipients.get("payments");
         Map<String, Double> payments = new LinkedHashMap<String, Double>();
         double allMoney = 0;
@@ -104,6 +113,11 @@ public class CoinSenderMain {
                 while (!done) {
                     System.out.println("Number of chunks: " + splitPayment.size());
                     System.out.println("You have already paid for these chunks: " + paidChunks.toString());
+                    String chunkString = "Chunks: ";
+                    for(int i = 0; i < splitPayment.size(); i++) {
+                    	chunkString += (i + 1) + ". " + (i * PART_SIZE + 1) + "-" + ((i + 1) * PART_SIZE ) + ", ";
+                    }
+                    System.out.println(chunkString);
                     System.out.print("Enter the number of chunk you want to pay for ('-1' to exit): ");
                     int chunk = in.nextInt() - 1;
                     if (chunk == -2) {
@@ -120,28 +134,43 @@ public class CoinSenderMain {
                         System.out.println("You've already paid for this chunk!");
                         continue;
                     }
-                    Map<String, Double> currentPayment = splitPayment.get(chunk);
+                    Map<String, Double> paymentToPay = splitPayment.get(chunk);
+                    Map<String, Double> currentPayment = new LinkedHashMap<>();
                     System.out.println("All payments: ");
                     double total = 0l;
-                    for (String key : currentPayment.keySet()) {
-                        total += currentPayment.get(key);
-                        String currentPaymentString = String.format("%.12f", currentPayment.get(key));
-                        System.out.println("Address: " + key + ", BTC: " + currentPaymentString);
+                    int id = 0;
+                    for (String key : paymentToPay.keySet()) {
+                    	id++;
+                    	Double toPay = paymentToPay.get(key);
+                        String currentPaymentString = String.format("%.12f", toPay);
+                        int addId = chunk * PART_SIZE + id;
+                        if(toPay >= MIN_PAY) {
+                        	total += toPay;
+                        	currentPayment.put(key, toPay);
+                        } else {
+                        	currentPaymentString += " [NOT PAID - less than minimal] ";
+                        }
+                        System.out.println(addId + ". Address: " + key + ", BTC: " + currentPaymentString);
+                        
                     }
                     Scanner scanner = new Scanner(System.in);
                     String totalString = String.format("%.12f", total);
+                    
                     System.out.println("Total: " + totalString);
                     System.out.println();
                     api.printFeeForTransaction(currentPayment);
                     System.out.println();
+                    int chunkUI = (chunk + 1);
+                    System.out.println("Prepare to pay for chunk " + chunkUI + " (" + (chunk * PART_SIZE + 1) + "-"
+							+ ((chunk + 1)* PART_SIZE ) + ") ...");
                     System.out.print("Are you sure you want to pay " + totalString + " BTC? [y/n]: ");
                     String answer = scanner.nextLine();
 
                     if (!answer.toLowerCase().equals("y")) {
                         continue;
                     }
-                    int chunkUI = (chunk + 1);
-                    System.out.println("Paying for chunk " + chunkUI + "...");
+					System.out.println("Paying for chunk " + chunkUI + " (" + (chunk * PART_SIZE + 1) + "-"
+							+ ((chunk + 1)* PART_SIZE) + ") ...");
 
                     Withdrawal withdrawal = null;
                     try {
@@ -172,7 +201,7 @@ public class CoinSenderMain {
 
     }
 
-    private static List splitResults(Map<String, Double> map) {
+	private static List splitResults(Map<String, Double> map) {
         List<Map<String, Double>> res = new ArrayList<Map<String, Double>>();
         Map<String, Double> part = new LinkedHashMap<String, Double>();
         for (String key : map.keySet()) {
