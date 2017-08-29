33e10d2cb462be77c3529c8b5af29ae5bce35765
Victor Shcherb <victor.shcherb@gmail.com>
2017-08-29 22:30:29 +0200

    Group by month as well

--- OsmAndMapCreator/src/net/osmand/data/diff/ObfDiffMerger.java
+++ OsmAndMapCreator/src/net/osmand/data/diff/ObfDiffMerger.java
@@ -2,15 +2,26 @@ package net.osmand.data.diff;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
 
 import net.osmand.util.Algorithms;
 import rtree.RTreeException;
 
 public class ObfDiffMerger {
+	static SimpleDateFormat day = new SimpleDateFormat("yyyy_MM_dd");
+	static SimpleDateFormat month = new SimpleDateFormat("yyyy_MM");
+	static {
+		day.setTimeZone(TimeZone.getTimeZone("UTC"));
+		month.setTimeZone(TimeZone.getTimeZone("UTC"));
+	}
 	
 	public static void main(String[] args) {
 		try {
@@ -26,21 +37,23 @@ public class ObfDiffMerger {
 	public static void mergeBulkOsmLiveDay(String location) {
 		try {
 			File folder = new File(location);
-			for (File region : folder.listFiles()) {
+			for (File region : getSortedFiles(folder)) {
 				if (!region.isDirectory()) {
 					continue;
 				}
-				System.out.println("Process " + region + " .");
 				String regionName = Algorithms.capitalizeFirstLetter(region.getName());
 				if (regionName.equals("_diff")) {
 					regionName = "World";
 				}
-				for (File date : region.listFiles()) {
+				for (File date : getSortedFiles(region)) {
 					if (!date.isDirectory()) {
 						continue;
 					}
 					File flToMerge = new File(region, regionName + "_" + date.getName() + ".obf.gz");
-					new ObfDiffMerger().process(flToMerge, Arrays.asList(date), true);
+					boolean processed = new ObfDiffMerger().process(flToMerge, Arrays.asList(date), true);
+					if(processed) {
+						System.out.println("Processed " + region + ".");
+					}
 				}
 			}
 		} catch (Exception e) {
@@ -49,11 +62,86 @@ public class ObfDiffMerger {
 		}
 	}
 	
-	public static void mergeBulkOsmLiveMonth(String location) {
-		// TODO Auto-generated method stub
+	private static List<File> getSortedFiles(File region) {
+		List<File> f = new ArrayList<>();
+		for(File l : region.listFiles()) {
+			f.add(l);
+		}
+		f.sort(new Comparator<File>() {
+
+			@Override
+			public int compare(File o1, File o2) {
+				return o1.getName().compareTo(o2.getName());
+			}
+			
+		});
 		
+		return f;
+	}
+
+
+	public static void mergeBulkOsmLiveMonth(String location) {
+		try {
+			Date currentDate = new Date();
+			String cdate = day.format(currentDate);
+			String cmnt = month.format(currentDate);
+			File folder = new File(location);
+			for (File region : getSortedFiles(folder)) {
+				if (!region.isDirectory()) {
+					continue;
+				}
+				String regionName = Algorithms.capitalizeFirstLetter(region.getName());
+				if (regionName.equals("_diff")) {
+					regionName = "World";
+				}
+				List<File> days = getSortedFiles(region);
+				
+				Map<String, List<File>> fls = groupFilesByMonth(regionName, days, cdate);
+				for (String fl : fls.keySet()) {
+					File flToMerge = new File(region, fl);
+					boolean processed = new ObfDiffMerger().process(flToMerge, fls.get(fl), true);
+					if(processed) {
+						String s = "";
+						for(File f: fls.get(fl)) {
+							s += f.getName() + " ";
+						}
+						System.out.println("Processed " + flToMerge + " with " + s);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}		
 	}
 	
+	private static Map<String, List<File>> groupFilesByMonth(String regionName, List<File> days, String cdate) {
+		Map<String, List<File>> grpFiles = new LinkedHashMap<String, List<File>>();
+		for (File d : days) {
+			if (!d.isFile() || !d.getName().startsWith(regionName + "_") || !d.getName().endsWith(".obf.gz")) {
+				continue;
+			}
+			// month
+			if (d.getName().endsWith("_00.obf.gz")) {
+				continue;
+			}
+			// current date
+			if (d.getName().endsWith(cdate + ".obf.gz")) {
+				continue;
+			}
+			String date = d.getName().substring(regionName.length() + 1, d.getName().length() - ".obf.gz".length());
+			String mnth = date.substring(0, date.length() - 2) + "00";
+			String mnthFile = regionName + "_" + mnth +".obf.gz";
+			if(!grpFiles.containsKey(mnthFile)) {
+				grpFiles.put(mnthFile, new ArrayList<File>());
+			}
+			grpFiles.get(mnthFile).add(d);
+
+		}
+		return grpFiles;
+	}
+
+
 	private void sortByDate(List<File> f) {
 		f.sort(new Comparator<File>() {
 
@@ -84,7 +172,7 @@ public class ObfDiffMerger {
 		process(result, inputDiffs, checkTimestamps);
 	}
 
-	public void process(File result, List<File> inputDiffs, boolean checkTimestamps) throws IOException,
+	public boolean process(File result, List<File> inputDiffs, boolean checkTimestamps) throws IOException,
 			RTreeException {
 		List<File> diffs = new ArrayList<>();
 		for(File fl : inputDiffs) {
@@ -114,12 +202,13 @@ public class ObfDiffMerger {
 				}
 			}
 			if(skipEditing) {
-				return;
+				return false;
 			}
 		}
 		ObfFileInMemory context = new ObfFileInMemory();
 		context.readObfFiles(diffs);
 		context.writeFile(result);
+		return true;
 	}
 
 
