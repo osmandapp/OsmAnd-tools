cf21a319bf0def56f3568d5a5000b8478ebdc526
Victor Shcherb <victor.shcherb@gmail.com>
2017-11-01 00:33:28 +0100

    Pass dbmonth for connection

--- obf-generation/changesets/generate_reports.sh
+++ obf-generation/changesets/generate_reports.sh
@@ -4,10 +4,10 @@ if [ -z "$PERIOD" ]; then
   PERIOD=$(date -d "$(date +%Y-%m-01) -1 day" "+%Y-%m")
 fi
 if [ -z "$EUR_VALUE" ]; then 
-  curl -X POST "http://builder.osmand.net/reports/all_reports.php?month=$PERIOD"
+  curl -X POST "http://builder.osmand.net/reports/all_reports.php?month=${PERIOD}&dbmonth=${PERIOD}"
 else 
   echo "EUR $EUR_VALUE / BTC $BTC_VALUE"
-  curl -X POST "http://builder.osmand.net/reports/all_reports.php?month=${PERIOD}&eurValue=${EUR_VALUE}&btcValue=${BTC_VALUE}"
+  curl -X POST "http://builder.osmand.net/reports/all_reports.php?month=${PERIOD}&dbmonth=${PERIOD}&eurValue=${EUR_VALUE}&btcValue=${BTC_VALUE}"
   DB_NAME=changeset_${PERIOD//-/_}
   DUMPNAME=${DB_NAME}_$(date +%s)
   pg_dump -U $DB_USER -v -F t -f $DUMPNAME.tar $DB_NAME
