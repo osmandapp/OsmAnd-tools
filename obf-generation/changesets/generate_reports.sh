ff9c92a8f206447df018a0a16685c9112127601c
Victor Shcherb <victor.shcherb@gmail.com>
2017-11-01 00:56:35 +0100

    Generate report quicker

--- obf-generation/changesets/generate_reports.sh
+++ obf-generation/changesets/generate_reports.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-cd /home/changesets/dumps
 if [ -z "$PERIOD" ]; then 
   PERIOD=$(date -d "$(date +%Y-%m-01) -1 day" "+%Y-%m")
 fi
+cd /var/www-download/reports/
 if [ -z "$EUR_VALUE" ]; then 
-  curl -X POST "http://builder.osmand.net/reports/all_reports.php?month=${PERIOD}&dbmonth=${PERIOD}"
+  # curl -X POST "http://builder.osmand.net/reports/all_reports.php?month=${PERIOD}&dbmonth=${PERIOD}"
+  php all_reports.php "${PERIOD}" 
 else 
   echo "EUR $EUR_VALUE / BTC $BTC_VALUE"
-  curl -X POST "http://builder.osmand.net/reports/all_reports.php?month=${PERIOD}&dbmonth=${PERIOD}&eurValue=${EUR_VALUE}&btcValue=${BTC_VALUE}"
+  # curl -X POST "http://builder.osmand.net/reports/all_reports.php?month=${PERIOD}&dbmonth=${PERIOD}&eurValue=${EUR_VALUE}&btcValue=${BTC_VALUE}"
+  php all_reports.php "${PERIOD}" "${EUR_VALUE}" "${BTC_VALUE}"
+  cd /home/changesets/dumps
   DB_NAME=changeset_${PERIOD//-/_}
   DUMPNAME=${DB_NAME}_$(date +%s)
   pg_dump -U $DB_USER -v -F t -f $DUMPNAME.tar $DB_NAME
