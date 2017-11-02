5a3fd1ae6c0fae2c4266c15af1ef0bdd5850bcf1
Victor Shcherb <victor.shcherb@gmail.com>
2017-11-02 10:25:37 +0100

    Truncate on rotate db

--- obf-generation/changesets/rotate_db.sh
+++ obf-generation/changesets/rotate_db.sh
@@ -11,6 +11,7 @@ pg_dump -U $DB_USER -v -F t -f $DB_NAME.tar changeset
 pg_restore -n public -U $DB_USER -v $DB_NAME.tar -d $DB_NAME
 psql -d $DB_NAME -U $DB_USER -c "DELETE FROM changesets where substr(closed_at_day, 0, 8) <> '$PERIOD'";
 psql -d $DB_NAME -U $DB_USER -c "DELETE FROM changeset_country c where not exists (select 1 from changesets d where d.id = c.changesetid)";
+psql -d $DB_NAME -U $DB_USER -c "TRUNCATE final_reports;";
 psql -d $DB_NAME -U $DB_USER -c "VACUUM FULL";
 # set everything
 pg_dump -U $DB_USER -v -F t -f $DB_NAME.tar $DB_NAME
