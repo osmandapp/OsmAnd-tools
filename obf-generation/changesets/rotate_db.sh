4af349a4afa7d1ece98b55f188609ed93ed09585
Victor Shcherb <victor.shcherb@gmail.com>
2017-11-03 00:49:47 +0100

    Update rotation

--- obf-generation/changesets/rotate_db.sh
+++ obf-generation/changesets/rotate_db.sh
@@ -12,6 +12,8 @@ pg_restore -p 5433 -n public -U $DB_USER -v $DB_NAME.tar -d $DB_NAME
 psql -p 5433 -d $DB_NAME -U $DB_USER -c "DELETE FROM changesets where substr(closed_at_day, 0, 8) <> '$PERIOD'";
 psql -p 5433 -d $DB_NAME -U $DB_USER -c "DELETE FROM changeset_country c where not exists (select 1 from changesets d where d.id = c.changesetid)";
 psql -p 5433 -d $DB_NAME -U $DB_USER -c "TRUNCATE final_reports;";
+psql -p 5433 -d $DB_NAME -U $DB_USER -c "REFRESH MATERIALIZED VIEW changesets_view;";
+psql -p 5433 -d $DB_NAME -U $DB_USER -c "REFRESH MATERIALIZED VIEW changeset_country_view;";
 psql -p 5433 -d $DB_NAME -U $DB_USER -c "VACUUM FULL";
 # set everything
 pg_dump -p 5433 -U $DB_USER -v -F t -f $DB_NAME.tar $DB_NAME
