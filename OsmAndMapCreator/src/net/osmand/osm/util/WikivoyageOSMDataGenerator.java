610ee2d0e39c9bd6b79e373084e79c04e24aa577
PaulStets <stetsenko.pavlo@gmail.com>
2018-04-08 12:03:35 +0300

    Complete osm data fetching

--- OsmAndMapCreator/src/net/osmand/osm/util/WikivoyageOSMDataGenerator.java
+++ OsmAndMapCreator/src/net/osmand/osm/util/WikivoyageOSMDataGenerator.java
@@ -8,22 +8,18 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 
 import net.osmand.PlatformUtil;
-import net.osmand.ResultMatcher;
-import net.osmand.CollatorStringMatcher.StringMatcherMode;
-import net.osmand.binary.BinaryMapAddressReaderAdapter;
 import net.osmand.binary.BinaryMapDataObject;
 import net.osmand.binary.BinaryMapIndexReader;
 import net.osmand.binary.BinaryMapIndexReader.SearchRequest;
 import net.osmand.data.Amenity;
-import net.osmand.data.City;
 import net.osmand.data.LatLon;
-import net.osmand.data.MapObject;
-import net.osmand.data.Street;
 import net.osmand.data.preparation.DBDialect;
 import net.osmand.map.OsmandRegions;
 import net.osmand.map.WorldRegion;
@@ -32,8 +28,14 @@ import net.osmand.util.MapUtils;
 
 public class WikivoyageOSMDataGenerator {
 	
+	private static int sleft = MapUtils.get31TileNumberX(-179.9);
+	private static int sright = MapUtils.get31TileNumberX(179.9);
+	private static int stop = MapUtils.get31TileNumberY(85);
+	private static int sbottom = MapUtils.get31TileNumberY(-85);
+	
 	private static final Log log = PlatformUtil.getLog(WikivoyageOSMDataGenerator.class);
 	private static final String[] columns = new String[] {"osm_id long", "city_type text", "population text", "country text", "region text"};
+	private static final double DISTANCE_THRESHOLD = 3000000;
 	private static OsmandRegions regions;
 	
 	public static void main(String[] args) throws SQLException, IOException {
@@ -68,6 +70,101 @@ public class WikivoyageOSMDataGenerator {
 	private static void processDb(File sqliteFile, File citiesObf) throws SQLException, IOException {
 		DBDialect dialect = DBDialect.SQLITE;
 		Connection conn = (Connection) dialect.getDatabaseConnection(sqliteFile.getAbsolutePath(), log);
+		addColumns(conn);
+		RandomAccessFile raf = new RandomAccessFile(citiesObf, "r");
+		BinaryMapIndexReader reader = new BinaryMapIndexReader(raf, citiesObf);
+		PreparedStatement stat = conn.prepareStatement("SELECT title, lat, lon FROM wikivoyage_articles");
+		int columns = getRowCount(conn);
+		int count = 0;
+		ResultSet rs = stat.executeQuery();
+		Map<String, List<Amenity>> cities = fetchCities(reader);
+		while(rs.next()) {
+			String title = rs.getString(1);
+			String searchTitle = title.replaceAll("\\(.*\\)", "").trim();
+			long lat = rs.getLong(2);
+			long lon = rs.getLong(3);
+			LatLon ll = new LatLon(lat, lon);
+			List<Amenity> results = cities.get(searchTitle);
+			Amenity acceptedResult = (results != null && results.size() == 1) ? results.get(0) : getClosestMatch(results, ll);
+			insertData(conn, title, acceptedResult, ll);
+			if (count++ % 500 == 0) {
+				System.out.format("%.2f", (((double)count / (double)columns) * 100d));
+				System.out.println("%");
+			}
+		}
+		stat.close();
+    	conn.close();
+    	raf.close();
+	}
+
+	private static void insertData(Connection conn, String title, Amenity acceptedResult, LatLon fromDB)
+			throws SQLException, IOException {
+		boolean hasResult = acceptedResult != null;
+		String sqlStatement = hasResult ? "UPDATE wikivoyage_articles SET lat = ?, lon = ?, osm_id = ?, city_type = ?, population = ?, "
+				+ "country = ?, region = ? WHERE title = ?" : "UPDATE wikivoyage_articles SET country = ?, region = ? WHERE title = ?";
+		PreparedStatement updateStatement = conn.prepareStatement(sqlStatement);
+		int column = 1;
+		LatLon coords = hasResult ? acceptedResult.getLocation() : fromDB;
+		if (hasResult) {
+			updateStatement.setDouble(column++, coords.getLatitude());
+			updateStatement.setDouble(column++, coords.getLongitude());
+			updateStatement.setLong(column++, acceptedResult.getId() >> 1);
+			updateStatement.setString(column++, acceptedResult.getSubType());
+			updateStatement.setString(column++, acceptedResult.getAdditionalInfo("population"));
+		}
+		List<String> regionsList = getRegions(coords.getLatitude(), coords.getLongitude());
+		WorldRegion country = null;
+		WorldRegion region = null;
+		for (String reg : regionsList) {
+			WorldRegion regionData = regions.getRegionDataByDownloadName(reg);
+			if (regionData == null) {
+				continue;
+			}
+			if (regionData.getLevel() == 2) {
+				country = regionData;
+			} else if (regionData.getLevel() > 2) {
+				region = regionData;
+			}
+		}
+		updateStatement.setString(column++, country == null ? null : country.getLocaleName());
+		updateStatement.setString(column++, region == null ? null : region.getRegionDownloadName());
+		updateStatement.setString(column++, title);
+		updateStatement.executeUpdate();
+		updateStatement.clearParameters();
+	}
+
+	private static Map<String, List<Amenity>> fetchCities(BinaryMapIndexReader reader) throws IOException {
+		Map<String, List<Amenity>> res = new HashMap<>();
+		SearchRequest<Amenity> req = BinaryMapIndexReader.buildSearchPoiRequest(sleft, sright, stop, sbottom, -1, BinaryMapIndexReader.ACCEPT_ALL_POI_TYPE_FILTER, null);
+		System.out.println("Start fetching cities");
+		long startTime = System.currentTimeMillis();
+		List<Amenity> results = reader.searchPoi(req);
+		System.out.println("Getting cities list took: " + (System.currentTimeMillis() - startTime) + " ms");
+		for (Amenity am : results) {
+			for (String name : am.getAllNames()) {
+				if (!res.containsKey(name)) {
+					List<Amenity> list = new ArrayList<>();
+					list.add(am);
+					res.put(name, list);
+				} else {
+					res.get(name).add(am);
+				}
+			}
+		}
+		return res;
+	}
+
+	private static int getRowCount(Connection conn) throws SQLException {
+		PreparedStatement ps = conn.prepareStatement("SELECT Count(*) FROM wikivoyage_articles");
+		int columns = 0;
+		ResultSet res = ps.executeQuery();
+		while (res.next()) {
+			columns = res.getInt(1);
+		}
+		return columns;
+	}
+
+	private static void addColumns(Connection conn) {
 		for (String s : columns) {
 			try {
 				conn.createStatement().execute("ALTER TABLE wikivoyage_articles ADD COLUMN " + s);
@@ -75,69 +172,10 @@ public class WikivoyageOSMDataGenerator {
 				System.out.println("Column alredy exsists");
 			}
 		}
-		RandomAccessFile raf = new RandomAccessFile(citiesObf, "r");
-		BinaryMapIndexReader reader = new BinaryMapIndexReader(raf, citiesObf);
-		PreparedStatement stat = conn.prepareStatement("SELECT title, lat, lon FROM wikivoyage_articles");
-		PreparedStatement updateStatement = conn.prepareStatement("UPDATE wikivoyage_articles SET lat = ?, lon = ?, osm_id = ?, city_type = ?, population = ?, "
-				+ "country = ?, region = ? WHERE title = ?");
-		PreparedStatement ps = conn.prepareStatement("SELECT Count(*) FROM wikivoyage_articles");
-		int columns = 0;
-		ResultSet res = ps.executeQuery();
-		while (res.next()) {
-			columns = res.getInt(1);
-		}
-		int count = 0;
-		ResultSet rs = stat.executeQuery();
-		while(rs.next()) {
-			String title = rs.getString(1);
-			SearchRequest<Amenity> req = BinaryMapIndexReader.buildSearchPoiRequest(0, 0, title,
-					0, Integer.MAX_VALUE, 0, Integer.MAX_VALUE, null);
-			reader.searchPoiByName(req);
-			long lat = rs.getLong(2);
-			long lon = rs.getLong(3);
-			LatLon ll = new LatLon(lat, lon);
-			List<Amenity> results = req.getSearchResults();
-			Amenity acceptedResult = results.size() == 1 ? results.get(0) : getClosestMatch(results, ll);
-			if (acceptedResult != null) {
-				int column = 1;
-				LatLon coords = acceptedResult.getLocation();
-				updateStatement.setDouble(column++, coords.getLatitude());
-				updateStatement.setDouble(column++, coords.getLongitude());
-				updateStatement.setLong(column++, acceptedResult.getId() >> 1);
-				updateStatement.setString(column++, acceptedResult.getSubType());
-				updateStatement.setString(column++, acceptedResult.getAdditionalInfo("population"));
-				List<String> regionsList = getRegions(coords.getLatitude(), coords.getLongitude());
-				WorldRegion country = null;
-				WorldRegion region = null;
-				for (String reg : regionsList) {
-					WorldRegion regionData = regions.getRegionDataByDownloadName(reg);
-					if (regionData == null) {
-						continue;
-					}
-					if (regionData.getLevel() == 2) {
-						country = regionData;
-					} else if (regionData.getLevel() > 2) {
-						region = regionData;
-					}
-				}
-				updateStatement.setString(column++, country == null ? "" : country.getLocaleName());
-				updateStatement.setString(column++, region == null ? "" : region.getRegionDownloadName());				
-				updateStatement.setString(column++, title);
-				updateStatement.executeUpdate();
-				updateStatement.clearParameters();
-				if (count++ % 100 == 0) {
-					System.out.println((((double)count / (double)columns) * 100d) + "%");
-				}
-			}
-		}
-		updateStatement.close();
-		stat.close();
-    	conn.close();
-    	raf.close();
 	}
 
 	private static Amenity getClosestMatch(List<Amenity> results, LatLon fromDB) {
-		if (results.isEmpty()) {
+		if (results == null || results.isEmpty()) {
 			return null;
 		}
 		Amenity result = results.get(0);
@@ -150,7 +188,12 @@ public class WikivoyageOSMDataGenerator {
 				result = a;
 			}
 		}
-		return result;
+		LatLon loc = result.getLocation();
+		if ((((int) loc.getLongitude() == (int) fromDB.getLongitude()) && ((int) loc.getLatitude() == (int) fromDB.getLatitude()) 
+				|| distance < DISTANCE_THRESHOLD)) {
+			return result;
+		}
+		return null;	
 	}
 	
 	private static List<String> getRegions(double lat, double lon) throws IOException {
