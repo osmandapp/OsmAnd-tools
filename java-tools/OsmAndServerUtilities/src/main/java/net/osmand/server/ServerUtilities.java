93c3bb0086fe36fd1334f056613161404b9e516d
Victor Shcherb <victor.shcherb@gmail.com>
2018-06-16 13:17:19 +0200

    Add utility

--- java-tools/OsmAndServerUtilities/src/main/java/net/osmand/server/ServerUtilities.java
+++ java-tools/OsmAndServerUtilities/src/main/java/net/osmand/server/ServerUtilities.java
@@ -1,20 +1,26 @@
 package net.osmand.server;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.json.JSONException;
+
 import net.osmand.bitcoinsender.TransactionAnalyzer;
+import net.osmand.live.subscriptions.UpdateSubscription;
 
 public class ServerUtilities {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, JSONException, ClassNotFoundException, SQLException {
 		String utl = args[0];
 		List<String> subArgs = new ArrayList<String>(Arrays.asList(args).subList(1, args.length));
 		String[] subArgsArray = subArgs.toArray(new String[args.length - 1]);
 		if (utl.equals("generate-report-underpaid")) {
 			TransactionAnalyzer.main(subArgsArray);
+		} else if (utl.equals("update-subscriptions")) {
+			UpdateSubscription.main(subArgsArray);
 		}
 	}
 }
