e7cb782fb5343c7e423141f37260a621f4abe0ba
Victor Shcherb <victor.shcherb@gmail.com>
2018-06-16 13:37:07 +0200

    Update server utilities

--- java-tools/OsmAndServerUtilities/src/main/java/net/osmand/server/ServerUtilities.java
+++ java-tools/OsmAndServerUtilities/src/main/java/net/osmand/server/ServerUtilities.java
@@ -7,6 +7,7 @@ import java.util.List;
 import net.osmand.bitcoinsender.TransactionAnalyzer;
 import net.osmand.data.changeset.CalculateCountryForChangesets;
 import net.osmand.live.subscriptions.UpdateSubscription;
+import net.osmand.mailsender.EmailSenderMain;
 
 public class ServerUtilities {
 
@@ -18,8 +19,13 @@ public class ServerUtilities {
 			TransactionAnalyzer.main(subArgsArray);
 		} else if (utl.equals("update-subscriptions")) {
 			UpdateSubscription.main(subArgsArray);
+		} else if (utl.equals("send-email")) {
+			EmailSenderMain.main(subArgsArray);
 		} else if (utl.equals("update-countries-for-changeset")) {
 			CalculateCountryForChangesets.main(subArgsArray);
+		} else {
+			System.err.println("Uknown command");
+			System.exit(1);
 		}
 	}
 }
