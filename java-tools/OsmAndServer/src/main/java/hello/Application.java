581162ecbb6c426145a9de60f0c004c0d8a5a6e6
Victor Shcherb <victor.shcherb@gmail.com>
2018-06-18 14:13:43 +0200

    Add simple app

--- java-tools/OsmAndServer/src/main/java/hello/Application.java
+++ java-tools/OsmAndServer/src/main/java/hello/Application.java
@@ -1,7 +1,5 @@
 package hello;
 
-import java.util.Arrays;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,13 +16,7 @@ public class Application {
     @Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
+			System.out.println("Application has started");
 		};
 	}
 
--- java-tools/OsmAndServer/src/main/java/hello/HelloController.java
+++ java-tools/OsmAndServer/src/main/java/hello/HelloController.java
@@ -1,14 +1,21 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import net.osmand.MapCreatorVersion;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.xmlpull.v1.XmlPullParserException;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public String index() throws IOException, SQLException, InterruptedException, XmlPullParserException {
+//    	IndexCreator.main(null);
+        return "Greetings from: " + MapCreatorVersion.APP_MAP_CREATOR_FULL_NAME+ " ";
     }
 
 }
