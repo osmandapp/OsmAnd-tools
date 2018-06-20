fb763663ec520e92c0c8de9dc5cffe9492c4ac27
crimean <alexey.kulish@osmand.net>
2018-06-20 14:20:58 +0300

    Added rolling log

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/controllers/HelloController.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/controllers/HelloController.java
@@ -2,6 +2,8 @@ package net.osmand.server.controllers;
 
 import net.osmand.MapCreatorVersion;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,7 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String index() {
+		LogFactory.getLog(HelloController.class).info("TEST LOG!!!");
         return "Greetings from: " + MapCreatorVersion.APP_MAP_CREATOR_FULL_NAME+ " ";
     }
 
