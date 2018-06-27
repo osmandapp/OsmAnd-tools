5401d22f7deb9fa323e67b217a0d91c784bf945c
Victor Shcherb <victor.shcherb@gmail.com>
2018-06-28 00:11:56 +0200

    Add build server

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/monitor/OsmAndServerMonitorTasks.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/monitor/OsmAndServerMonitorTasks.java
@@ -6,12 +6,17 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Set;
 import java.util.TimeZone;
+import java.util.TreeSet;
 
 import net.osmand.server.TelegramBotManager;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -28,11 +33,11 @@ public class OsmAndServerMonitorTasks {
 	@Autowired
 	TelegramBotManager telegram;
 
-	LiveCheckInfo live = new LiveCheckInfo(); 
-	
+	LiveCheckInfo live = new LiveCheckInfo();
+	BuildServerCheckInfo buildServer = new BuildServerCheckInfo();
 	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 	
-    @Scheduled(fixedRate = 10 * MINUTE)
+    @Scheduled(fixedRate = 5 * MINUTE)
     public void checkOsmAndLiveStatus() {
     	try {
 			URL url = new URL("http://osmand.net/api/osmlive_status");
@@ -50,14 +55,60 @@ public class OsmAndServerMonitorTasks {
 			live.lastCheckTimestamp = System.currentTimeMillis();
 			live.lastOsmAndLiveDelay = currentDelay;
 		} catch (Exception e) {
-			telegram.sendMonitoringAlertMessage("Exception while checking the server status.");
+			telegram.sendMonitoringAlertMessage("Exception while checking the server live status.");
+			LOG.error(e.getMessage(), e);
+		}
+    }
+    
+    
+    @Scheduled(fixedRate = 5 * MINUTE)
+    public void checkOsmAndBuildServer() {
+    	try {
+    		Set<String> jobsFailed = new TreeSet<String>();
+			URL url = new URL("http://builder.osmand.net:8080/api/json");
+			InputStream is = url.openConnection().getInputStream();
+			JSONObject object = new JSONObject(new JSONTokener(is));
+			JSONArray jsonArray = object.getJSONArray("jobs");
+			for(int i = 0; i < jsonArray.length(); i++) {
+				JSONObject jb = jsonArray.getJSONObject(i);
+				String name = jb.getString("name");
+				String color = jb.getString("color");
+				if(!color.equals("blue") && !color.equals("disabled")) {
+					jobsFailed.add(name);
+				}
+			}
+			is.close();
+			if(!buildServer.jobsFailed.equals(jobsFailed)) {
+				Set<String> jobsFailedCopy = new TreeSet<String>(jobsFailed);
+				jobsFailedCopy.removeAll(buildServer.jobsFailed);
+				if(!jobsFailedCopy.isEmpty()) {
+					telegram.sendMonitoringAlertMessage("There are new failures on OsmAnd Build Server: " + jobsFailedCopy);
+				}
+				
+				Set<String> jobsRecoveredCopy = new TreeSet<String>(buildServer.jobsFailed);
+				jobsRecoveredCopy.removeAll(jobsFailed);
+				if(!jobsRecoveredCopy.isEmpty()) {
+					telegram.sendMonitoringAlertMessage("There are recovered jobs on OsmAnd Build Server: " + jobsRecoveredCopy);
+				}
+				buildServer.jobsFailed = jobsFailed;
+			}
+			buildServer.lastCheckTimestamp = System.currentTimeMillis();
+		} catch (Exception e) {
+			telegram.sendMonitoringAlertMessage("Exception while checking the build server status.");
 			LOG.error(e.getMessage(), e);
 		}
     	
     }
+    
 
     public String getStatusMessage() {
-    	return getLiveDelayedMessage(live.lastOsmAndLiveDelay);
+    	String msg = getLiveDelayedMessage(live.lastOsmAndLiveDelay) +"\n";
+    	if(buildServer.jobsFailed.isEmpty()) {
+    		msg += "OsmAnd Build server is OK.";
+    	} else {
+    		msg += "OsmAnd Build server has failing jobs: " + buildServer.jobsFailed;
+    	}
+    	return msg;
     }
 
 	private String getLiveDelayedMessage(long delay) {
@@ -65,9 +116,14 @@ public class OsmAndServerMonitorTasks {
     	return "OsmAnd Live is delayed by " + (roundUp / 100.f) + " hours";
 	}
 	
-	private static class LiveCheckInfo {
+	protected static class LiveCheckInfo {
 		long previousOsmAndLiveDelay = 0;
 		long lastOsmAndLiveDelay = 0;
+		long lastCheckTimestamp = 0;
+	}
+	
+	protected static class BuildServerCheckInfo {
+		Set<String> jobsFailed = new TreeSet<String>(); 
 		long lastCheckTimestamp = 0;
 	}
 	
