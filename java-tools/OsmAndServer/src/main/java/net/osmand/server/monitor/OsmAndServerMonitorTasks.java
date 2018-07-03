8bc2b51d1c86b66ecb1d6c96bbbaea8b102a7ca4
yevhenii <fluffy27@nxt.ru>
2018-07-03 20:45:06 +0300

    fix notification spam issue, rename validation method, change notification messages

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/monitor/OsmAndServerMonitorTasks.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/monitor/OsmAndServerMonitorTasks.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.SimpleDateFormat;
@@ -32,190 +31,198 @@ import org.xmlpull.v1.XmlPullParserException;
 @Component
 public class OsmAndServerMonitorTasks {
 
-	private static final Log LOG = LogFactory.getLog(TelegramBotManager.class);
-	
-	private static final int SECOND = 1000;
-	private static final int MINUTE = 60 * SECOND;
-	private static final int HOUR = 60 * MINUTE;
-	private static final int LIVE_STATUS_MINUTES = 2;
-	
-	DescriptiveStatistics live3Hours = new DescriptiveStatistics(3 * 60 / LIVE_STATUS_MINUTES);
-	DescriptiveStatistics live24Hours = new DescriptiveStatistics(24 * 60 / LIVE_STATUS_MINUTES);
-	
-	@Autowired
-	OsmAndServerMonitoringBot telegram;
+    private static final Log LOG = LogFactory.getLog(TelegramBotManager.class);
 
-	LiveCheckInfo live = new LiveCheckInfo();
-	BuildServerCheckInfo buildServer = new BuildServerCheckInfo();
-	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-	
+    private static final int SECOND = 1000;
+    private static final int MINUTE = 60 * SECOND;
+    private static final int HOUR = 60 * MINUTE;
+    private static final int LIVE_STATUS_MINUTES = 2;
+
+    DescriptiveStatistics live3Hours = new DescriptiveStatistics(3 * 60 / LIVE_STATUS_MINUTES);
+    DescriptiveStatistics live24Hours = new DescriptiveStatistics(24 * 60 / LIVE_STATUS_MINUTES);
+
+    @Autowired
+    OsmAndServerMonitoringBot telegram;
+
+    LiveCheckInfo live = new LiveCheckInfo();
+    BuildServerCheckInfo buildServer = new BuildServerCheckInfo();
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+    boolean mapIndexPrevValidation = true;
+
     @Scheduled(fixedRate = LIVE_STATUS_MINUTES * MINUTE)
     public void checkOsmAndLiveStatus() {
-    	checkOsmAndLiveStatus(true);
+        checkOsmAndLiveStatus(true);
     }
-    
+
     public void checkOsmAndLiveStatus(boolean updateStats) {
-    	try {
-    		
-			URL url = new URL("http://osmand.net/api/osmlive_status");
-			InputStream is = url.openConnection().getInputStream();
-			BufferedReader br = new BufferedReader(new InputStreamReader(is));
-			String osmlivetime = br.readLine();
-			format.setTimeZone(TimeZone.getTimeZone("UTC"));
-			Date dt = format.parse(osmlivetime);
-			br.close();
-			long currentDelay = System.currentTimeMillis() - dt.getTime();
-			if(currentDelay - live.previousOsmAndLiveDelay > 30 * MINUTE && currentDelay > HOUR) {
-				telegram.sendMonitoringAlertMessage(getLiveDelayedMessage(currentDelay));
-				live.previousOsmAndLiveDelay = currentDelay;
-			}
-			live.lastCheckTimestamp = System.currentTimeMillis();
-			live.lastOsmAndLiveDelay = currentDelay;
-			if(updateStats) {
-				live3Hours.addValue(currentDelay);
-				live24Hours.addValue(currentDelay);
-			}
-		} catch (Exception e) {
-			telegram.sendMonitoringAlertMessage("Exception while checking the server live status.");
-			LOG.error(e.getMessage(), e);
-		}
+        try {
+
+            URL url = new URL("http://osmand.net/api/osmlive_status");
+            InputStream is = url.openConnection().getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String osmlivetime = br.readLine();
+            format.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Date dt = format.parse(osmlivetime);
+            br.close();
+            long currentDelay = System.currentTimeMillis() - dt.getTime();
+            if (currentDelay - live.previousOsmAndLiveDelay > 30 * MINUTE && currentDelay > HOUR) {
+                telegram.sendMonitoringAlertMessage(getLiveDelayedMessage(currentDelay));
+                live.previousOsmAndLiveDelay = currentDelay;
+            }
+            live.lastCheckTimestamp = System.currentTimeMillis();
+            live.lastOsmAndLiveDelay = currentDelay;
+            if (updateStats) {
+                live3Hours.addValue(currentDelay);
+                live24Hours.addValue(currentDelay);
+            }
+        } catch (Exception e) {
+            telegram.sendMonitoringAlertMessage("Exception while checking the server live status.");
+            LOG.error(e.getMessage(), e);
+        }
     }
-    
-    
+
+
     @Scheduled(fixedRate = MINUTE)
     public void checkOsmAndBuildServer() {
-    	try {
-    		Set<String> jobsFailed = new TreeSet<String>();
-			URL url = new URL("http://builder.osmand.net:8080/api/json");
-			InputStream is = url.openConnection().getInputStream();
-			JSONObject object = new JSONObject(new JSONTokener(is));
-			JSONArray jsonArray = object.getJSONArray("jobs");
-			for(int i = 0; i < jsonArray.length(); i++) {
-				JSONObject jb = jsonArray.getJSONObject(i);
-				String name = jb.getString("name");
-				String color = jb.getString("color");
-				if(!color.equals("blue") && !color.equals("disabled") && 
-						!color.equals("notbuilt") && !color.equals("blue_anime")) {
-					jobsFailed.add(name);
-				}
-			}
-			is.close();
-			if(!buildServer.jobsFailed.equals(jobsFailed)) {
-				Set<String> jobsFailedCopy = new TreeSet<String>(jobsFailed);
-				jobsFailedCopy.removeAll(buildServer.jobsFailed);
-				if(!jobsFailedCopy.isEmpty()) {
-					telegram.sendMonitoringAlertMessage("There are new failures on OsmAnd Build Server: " + jobsFailedCopy);
-				}
-				
-				Set<String> jobsRecoveredCopy = new TreeSet<String>(buildServer.jobsFailed);
-				jobsRecoveredCopy.removeAll(jobsFailed);
-				if(!jobsRecoveredCopy.isEmpty()) {
-					telegram.sendMonitoringAlertMessage("There are recovered jobs on OsmAnd Build Server: " + jobsRecoveredCopy);
-				}
-				buildServer.jobsFailed = jobsFailed;
-			}
-			buildServer.lastCheckTimestamp = System.currentTimeMillis();
-		} catch (Exception e) {
-			telegram.sendMonitoringAlertMessage("Exception while checking the build server status.");
-			LOG.error(e.getMessage(), e);
-		}
-    	
+        try {
+            Set<String> jobsFailed = new TreeSet<String>();
+            URL url = new URL("http://builder.osmand.net:8080/api/json");
+            InputStream is = url.openConnection().getInputStream();
+            JSONObject object = new JSONObject(new JSONTokener(is));
+            JSONArray jsonArray = object.getJSONArray("jobs");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jb = jsonArray.getJSONObject(i);
+                String name = jb.getString("name");
+                String color = jb.getString("color");
+                if (!color.equals("blue") && !color.equals("disabled") &&
+                        !color.equals("notbuilt") && !color.equals("blue_anime")) {
+                    jobsFailed.add(name);
+                }
+            }
+            is.close();
+            if (!buildServer.jobsFailed.equals(jobsFailed)) {
+                Set<String> jobsFailedCopy = new TreeSet<String>(jobsFailed);
+                jobsFailedCopy.removeAll(buildServer.jobsFailed);
+                if (!jobsFailedCopy.isEmpty()) {
+                    telegram.sendMonitoringAlertMessage("There are new failures on OsmAnd Build Server: " + jobsFailedCopy);
+                }
+
+                Set<String> jobsRecoveredCopy = new TreeSet<String>(buildServer.jobsFailed);
+                jobsRecoveredCopy.removeAll(jobsFailed);
+                if (!jobsRecoveredCopy.isEmpty()) {
+                    telegram.sendMonitoringAlertMessage("There are recovered jobs on OsmAnd Build Server: " + jobsRecoveredCopy);
+                }
+                buildServer.jobsFailed = jobsFailed;
+            }
+            buildServer.lastCheckTimestamp = System.currentTimeMillis();
+        } catch (Exception e) {
+            telegram.sendMonitoringAlertMessage("Exception while checking the build server status.");
+            LOG.error(e.getMessage(), e);
+        }
     }
 
     @Scheduled(fixedRate = 5 * MINUTE)
     public void checkIndexesValidity() {
-		GZIPInputStream gis = null;
-    	try {
-			URL url = new URL("http://osmand.net/get_indexes?gzip=true");
-			URLConnection conn = url.openConnection();
-			InputStream is = conn.getInputStream();
-			gis = new GZIPInputStream(is);
+        GZIPInputStream gis = null;
+        try {
+            URL url = new URL("http://osmand.net/get_indexes?gzip=true");
+            URLConnection conn = url.openConnection();
+            InputStream is = conn.getInputStream();
+            gis = new GZIPInputStream(is);
 
-			validateAndReport(gis);
+            boolean mapIndexCurrValidation = validateMapIndex(gis);
+            if (mapIndexPrevValidation && !mapIndexCurrValidation) {
+                telegram.sendMonitoringAlertMessage("Map index is not correctly generated on the website (check).");
+            }
 
-		} catch (IOException ioex) {
-			LOG.error(ioex.getMessage(), ioex);
-			telegram.sendMonitoringAlertMessage("Exception while checking the indexes.xml validity.");
-		} finally {
-			if (gis != null) {
-				close(gis);
-			}
-		}
-	}
+            if (!mapIndexPrevValidation && mapIndexCurrValidation) {
+                telegram.sendMonitoringAlertMessage("Map index is correct.");
+            }
+            mapIndexPrevValidation = mapIndexCurrValidation;
 
-	private void validateAndReport(InputStream is) throws IOException {
-		try {
-			XmlPullParser xpp = new KXmlParser();
-			xpp.setInput(new InputStreamReader(is));
+        } catch (IOException ioex) {
+            LOG.error(ioex.getMessage(), ioex);
+            telegram.sendMonitoringAlertMessage("Exception while checking the map index validity.");
+        } finally {
+            if (gis != null) {
+                close(gis);
+            }
+        }
+    }
 
-			int eventType = xpp.getEventType();
-			while (eventType != XmlPullParser.END_DOCUMENT) {
-				if (eventType == XmlPullParser.TEXT) {
-					if (!xpp.isWhitespace()) {
-						throw new XmlPullParserException("Text in document");
-					}
-				}
-				eventType = xpp.next();
-			}
+    private boolean validateMapIndex(InputStream is) throws IOException {
+        boolean valid = true;
+        try {
+            XmlPullParser xpp = new KXmlParser();
+            xpp.setInput(new InputStreamReader(is));
+            int eventType = xpp.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.TEXT) {
+                    if (!xpp.isWhitespace()) {
+                        throw new XmlPullParserException("Text in document");
+                    }
+                }
+                eventType = xpp.next();
+            }
+        } catch (XmlPullParserException ex) {
+            valid = false;
+            LOG.error(ex.getMessage(), ex);
+        }
+        return valid;
+    }
 
-			telegram.sendMonitoringAlertMessage("indexes.xml is well-formed.");
-		} catch (XmlPullParserException ex) {
-			LOG.error(ex.getMessage(), ex);
-			telegram.sendMonitoringAlertMessage("indexes.xml has invalid structure.");
-		}
-	}
+    private void close(InputStream is) {
+        try {
+            is.close();
+        } catch (IOException ex) {
+            LOG.error(ex.getMessage(), ex);
+        }
+    }
 
-	private void close(InputStream is) {
-    	try {
-    		is.close();
-		} catch (IOException ex) {
-    		LOG.error(ex.getMessage(), ex);
-		}
-	}
-    
 
     public String refreshAll() {
-    	checkOsmAndLiveStatus(false);
-    	checkOsmAndBuildServer();
-    	return getStatusMessage();
+        checkOsmAndLiveStatus(false);
+        checkOsmAndBuildServer();
+        return getStatusMessage();
     }
-    
+
     public String getStatusMessage() {
-    	String msg = getLiveDelayedMessage(live.lastOsmAndLiveDelay) +"\n";
-    	if(buildServer.jobsFailed.isEmpty()) {
-    		msg += "OsmAnd Build server is OK.";
-    	} else {
-    		msg += "OsmAnd Build server has failing jobs: " + buildServer.jobsFailed;
-    	}
-    	return msg;
+        String msg = getLiveDelayedMessage(live.lastOsmAndLiveDelay) + "\n";
+        if (buildServer.jobsFailed.isEmpty()) {
+            msg += "OsmAnd Build server is OK.";
+        } else {
+            msg += "OsmAnd Build server has failing jobs: " + buildServer.jobsFailed;
+        }
+        return msg;
     }
 
-	private String getLiveDelayedMessage(long delay) {
-		String txt = "OsmAnd Live is delayed by " + formatTime(delay) + " hours ";
-		txt += " (avg3h " + formatTime(live3Hours.getMean()) + ", avg24h " + formatTime(live24Hours.getMean())
-				+ ", max24h " + formatTime(live24Hours.getMax()) + ")";
-    	return txt;
-	}
-	
-	private String formatTime(double i) {
-		double f = i / HOUR;
-		int d = (int) f;
-		int min = (int) ((f - d) * 60);
-		if(min < 10) {
-			return d + ":0" + min;
-		}
-		return d + ":" + min;
-	}
+    private String getLiveDelayedMessage(long delay) {
+        String txt = "OsmAnd Live is delayed by " + formatTime(delay) + " hours ";
+        txt += " (avg3h " + formatTime(live3Hours.getMean()) + ", avg24h " + formatTime(live24Hours.getMean())
+                + ", max24h " + formatTime(live24Hours.getMax()) + ")";
+        return txt;
+    }
 
-	protected static class LiveCheckInfo {
-		long previousOsmAndLiveDelay = 0;
-		long lastOsmAndLiveDelay = 0;
-		long lastCheckTimestamp = 0;
-	}
-	
-	protected static class BuildServerCheckInfo {
-		Set<String> jobsFailed = new TreeSet<String>(); 
-		long lastCheckTimestamp = 0;
-	}
-	
+    private String formatTime(double i) {
+        double f = i / HOUR;
+        int d = (int) f;
+        int min = (int) ((f - d) * 60);
+        if (min < 10) {
+            return d + ":0" + min;
+        }
+        return d + ":" + min;
+    }
+
+    protected static class LiveCheckInfo {
+        long previousOsmAndLiveDelay = 0;
+        long lastOsmAndLiveDelay = 0;
+        long lastCheckTimestamp = 0;
+    }
+
+    protected static class BuildServerCheckInfo {
+        Set<String> jobsFailed = new TreeSet<String>();
+        long lastCheckTimestamp = 0;
+    }
+
 }
