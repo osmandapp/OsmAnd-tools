4a4b44e724b5ef73f6fb426a9ff1e3860fc74c7a
Victor Shcherb <victor.shcherb@gmail.com>
2018-09-14 00:55:29 +0200

    Add live reports

--- java-tools/OsmAndServerUtilities/src/main/java/net/osmand/data/changeset/OsmAndLiveReports.java
+++ java-tools/OsmAndServerUtilities/src/main/java/net/osmand/data/changeset/OsmAndLiveReports.java
@@ -85,19 +85,33 @@ public class OsmAndLiveReports {
 		return 20;
 	}
 	
-	private Number getEurBTCRate() throws IOException {
-		URLConnection un = new URL("https://blockchain.info/ticker").openConnection();
-		StringBuilder bld = Algorithms.readFromInputStream(un.getInputStream());
-		JSONObject obj = new JSONObject(bld.toString());
-		return obj.getJSONObject("EUR").getDouble("sell");
-	}
-
 	public int getRegionRankingRange() {
 		return 7;
 	}
 
 	public int getMinChanges() {
 		return 3;
+	}
+	
+	private double getEurBTCRate() throws IOException {
+		URLConnection un = new URL("https://blockchain.info/ticker").openConnection();
+		StringBuilder bld = Algorithms.readFromInputStream(un.getInputStream());
+		JSONObject obj = new JSONObject(bld.toString());
+		return obj.getJSONObject("EUR").getDouble("sell");
+	}
+
+	private double getBtcValue() throws IOException {
+		double rt = getEurBTCRate();
+		double eur = getEurValue();
+		if (rt != 0) {
+			return eur / rt;
+		}
+		return 0;
+	}
+
+
+	private double getEurValue() {
+		throw new UnsupportedOperationException();
 	}
 	
 	
@@ -187,22 +201,257 @@ public class OsmAndLiveReports {
 		return supportersReport;
 	}
 
+	public TotalChangesReport getTotalChanges(String region) throws SQLException {
+		TotalChangesReport report = new TotalChangesReport();
+		report.month = month;
+		report.region = region;
+		ResultSet rs;
+		if(!isEmpty(region)) {
+			String r = "select count(distinct ch.username) users, count(distinct ch.id) changes" + 
+					  " from changesets_view ch, changeset_country_view cc where ch.id = cc.changesetid"+ 
+					  " and cc.countryid = (select id from countries where downloadname= '?')"+
+					  " and substr(ch.closed_at_day, 0, 8) = '?'";
+			PreparedStatement ps = conn.prepareStatement(r);
+			ps.setString(1, region);
+			ps.setString(2, month);
+			rs = ps.executeQuery();
+		} else {
+			String r = "select count ( distinct username) users, count(*) changes from changesets_view"+
+						" where substr(closed_at_day, 0, 8) = '?'";
+			PreparedStatement ps = conn.prepareStatement(r);
+			ps.setString(1, month);
+			rs = ps.executeQuery();
+		}
+		rs.next();
+		report.users = rs.getInt("users");
+		report.changes = rs.getInt("changes");
+		return report;
+	}
 	
+	public RankingReport getRanking(String region) throws SQLException {
+		RankingReport report = new RankingReport();
+		report.month = month;
+		report.region = region;
+		int minChanges = getMinChanges();
+		int rankingRange;
+		ResultSet rs;
+		if(!isEmpty(region)) {
+			rankingRange = getRegionRankingRange();
+		    String r =  " SELECT data.cnt changes, count(*) group_size from ("+
+		    			"  		SELECT username, count(*) cnt from changesets_view ch, changeset_country_view cc " + 
+		    			"		WHERE substr(ch.closed_at_day, 0, 8) = '?' and ch.id = cc.changesetid  "+
+		    			"  			and cc.countryid = (select id from countries where downloadname= '?' )" +
+		    			" 		GROUP BY ch.username having count(*) >= ? order by count(*) desc )" +
+		    			" data group by data.cnt order by changes desc";
+			PreparedStatement ps = conn.prepareStatement(r);
+			ps.setString(1, month);
+			ps.setString(2, region);
+			ps.setInt(3, minChanges);
+			rs = ps.executeQuery();
+		} else {
+			rankingRange = getRankingRange();
+			String r = "SELECT data.cnt changes, count(*) group_size from ( "+
+					   "	SELECT username, count(*) cnt from changesets_view ch where substr(ch.closed_at_day, 0, 8) = ? " +
+					   " 	GROUP BY by ch.username having count(*) >= ? order by count(*) desc) " +
+					   " data group by data.cnt order by changes desc";
+			PreparedStatement ps = conn.prepareStatement(r);
+			ps.setString(1, month);
+			ps.setInt(2, minChanges);
+			rs = ps.executeQuery();
+		}
+		List<RankingRange> lst = report.rows;
+		while(rs.next()) {
+			RankingRange r = new RankingRange();
+			r.minChanges = rs.getInt(1);
+			r.maxChanges = rs.getInt(1);
+			r.countUsers = rs.getInt(2);
+			r.totalChanges = r.minChanges * r.countUsers;
+			lst.add(r);
+		}
+		while(lst.size() > rankingRange && lst.size() > 1) {
+			int minind = 0;
+			int minsum = lst.get(0).countUsers + lst.get(1).countUsers;
+			for(int i = 1 ; i < lst.size() - 1; i++){
+				int sum = lst.get(i).countUsers + lst.get(i+1).countUsers;
+				if(sum < minsum) {
+					minind = i;
+					minsum = sum;
+				}
+			}
+			int min = Math.min(lst.get(minind).minChanges, lst.get(minind+1).minChanges);
+			int max = Math.max(lst.get(minind).maxChanges, lst.get(minind+1).maxChanges);
+			int changes = lst.get(minind).totalChanges + lst.get(minind + 1).totalChanges;
+			lst.remove(minind);
+			lst.get(minind).minChanges = min;
+			lst.get(minind).maxChanges = max;
+			lst.get(minind).countUsers = minsum;
+			lst.get(minind).totalChanges = changes;
+		}
+		for(int i = 0; i < lst.size(); i++) {
+			RankingRange r = lst.get(i);
+			r.rank = i + 1;
+			if(r.countUsers > 0) {
+				r.avgChanges = r.totalChanges / ((float) r.countUsers);
+			}
+		}
+		return report;
+	}
+
+//		function calculateUserRanking($useReport = true, $saveReport = 0 ) {
+//		  global $iregion, $imonth, $iuser, $month, $dbconn;
+//		  $finalReport = getReport('calculateUserRanking', $iregion);
+//		  if($finalReport != NULL && $useReport ) {
+//		    return $finalReport;
+//		  }
+//		  $gar = calculateRanking('', $useReport, -1)->rows;
+//		  $ar = calculateRanking(NULL, $useReport, -1)->rows;
+//		  $region =  pg_escape_string($iregion);
+//		  $user =  pg_escape_string($iuser);
+//		  
+//		    
+//		  $result = pg_query($dbconn, "
+//		    SELECT  t.username, t.size changes , s.size gchanges FROM
+//		     ( SELECT username, count(*) size 
+//		        from changesets_view ch, changeset_country_view cc where ch.id = cc.changesetid 
+//		        and substr(ch.closed_at_day, 0, 8) = '{$month}'
+//		        and cc.countryid = (select id from countries where downloadname= '${region}')
+//		        and username= '${user}'
+//		        group by ch.username) t join 
+//		     (SELECT username, count(*) size from changesets_view ch where 
+//		      substr(ch.closed_at_day, 0, 8) = '{$month}'
+//		      and username= '${user}'
+//		      group by ch.username
+//		      ) s on s.username = t.username order by t.size desc;
+//		        ");
+//		  if (!$result) {
+//		    $res = new stdClass();
+//		    $res->error ='No result';
+//		    return $res;
+//		  }
+//		  
+//		  
+//		  $res = new stdClass();
+//		  $res->month = $month;
+//		  $res->rows = array();
+//		  while ($row = pg_fetch_row($result)) {
+//		    $rw = new stdClass();
+//		    array_push($res->rows, $rw);
+//		    $rw->user = $row[0];
+//		    $rw->changes = $row[1];
+//		    $rw->globalchanges = $row[2];
+//		    $rw->rank = '';
+//		    for($i = 0; $i < count($ar); $i++) {
+//		      if($ar[$i]->minChanges <= $row[1]  && $ar[$i]->maxChanges >= $row[1] ){
+//		        $rw->rank = $ar[$i]->rank;
+//		        // $rw->min = $ar[$i]->minChanges ;
+//		        // $rw->max = $ar[$i]->maxChanges ;
+//		        break;
+//		      }
+//		    }
+//		    $rw->grank = '';
+//		    for($i = 0; $i < count($gar); $i++) {
+//		      if($gar[$i]->minChanges <= $row[2]  && $gar[$i]->maxChanges >= $row[2] ){
+//		        $rw->grank = $gar[$i]->rank;
+//		        // $rw->gmin = $gar[$i]->minChanges ;
+//		        // $rw->gmax = $gar[$i]->maxChanges ;
+//
+//		        break;
+//		      }
+//		    }
+//		  }
+//		  if($saveReport >= 0) {
+//		    saveReport('calculateUserRanking', $res, $imonth, $iregion, $timeReport);
+//		  }
+//		  return $res;
+//
+//		}
+//
+//
+//		function calculateUsersRanking($useReport = true, $saveReport = 0) {
+//		  global $iregion, $imonth, $month, $dbconn;
+//		  $finalReport = getReport('calculateUsersRanking', $iregion);
+//		  if($finalReport != NULL && $useReport) {
+//		    return $finalReport;
+//		  }
+//		  $gar = calculateRanking('', true, -1)->rows;
+//		  $ar = calculateRanking(NULL, true, -1)->rows;
+//		  $region =  pg_escape_string($iregion);
+//		  $min_changes = getMinChanges();
+//		  
+//		    
+//		  $result = pg_query($dbconn, "
+//		    SELECT  t.username, t.size changes , s.size gchanges FROM
+//		     ( SELECT username, count(*) size 
+//		        from changesets_view ch, changeset_country_view cc where ch.id = cc.changesetid 
+//		        and substr(ch.closed_at_day, 0, 8) = '{$month}'
+//		        and cc.countryid = (select id from countries where downloadname= '${region}')
+//		        group by ch.username
+//		        having count(*) >= {$min_changes}
+//		        order by count(*) desc ) t join 
+//		     (SELECT username, count(*) size from changesets_view ch where 
+//		      substr(ch.closed_at_day, 0, 8) = '{$month}'
+//		      group by ch.username
+//		      ) s on s.username = t.username order by t.size desc;
+//		        ");
+//		  if (!$result) {
+//		    $res = new stdClass();
+//		    $res->error ='No result';
+//		    return $res;
+//		  }
+//		  
+//		  
+//		  $res = new stdClass();
+//		  $res->month = $month;
+//		  $res->rows = array();
+//		  while ($row = pg_fetch_row($result)) {
+//		    $rw = new stdClass();
+//		    array_push($res->rows, $rw);
+//		    $rw->user = $row[0];
+//		    $rw->changes = $row[1];
+//		    $rw->globalchanges = $row[2];
+//		    $rw->rank = '';
+//		    for($i = 0; $i < count($ar); $i++) {
+//		      if($ar[$i]->minChanges <= $row[1]  && $ar[$i]->maxChanges >= $row[1] ){
+//		        $rw->rank = $ar[$i]->rank;
+//		        // $rw->min = $ar[$i]->minChanges ;
+//		        // $rw->max = $ar[$i]->maxChanges ;
+//		        break;
+//		      }
+//		    }
+//		    $rw->grank = '';
+//		    for($i = 0; $i < count($gar); $i++) {
+//		      if($gar[$i]->minChanges <= $row[2]  && $gar[$i]->maxChanges >= $row[2] ){
+//		        $rw->grank = $gar[$i]->rank;
+//		        // $rw->gmin = $gar[$i]->minChanges ;
+//		        // $rw->gmax = $gar[$i]->maxChanges ;
+//
+//		        break;
+//		      }
+//		    }
+//		    
+//		  }
+//		  if($saveReport >= 0) {
+//		    saveReport('calculateUsersRanking', $res, $imonth, $iregion, $saveReport);
+//		  }
+//		  return $res;
+//
+//		}
+
 
 	
-	private String getJsonReport(OsmAndLiveReportType type) throws SQLException {
+	public String getJsonReport(OsmAndLiveReportType type, String region) throws SQLException {
 		Gson gson = new Gson();
 		if(type == OsmAndLiveReportType.COUNTRIES) {
 			return gson.toJson(getCountries());
 		} else if(type == OsmAndLiveReportType.SUPPORTERS) {
 			return gson.toJson(getSupporters());
 		} else if(type == OsmAndLiveReportType.TOTAL_CHANGES) {
-			throw new UnsupportedOperationException();
+			return gson.toJson(getTotalChanges(region));
+		} else if(type == OsmAndLiveReportType.RANKING) {
+			return gson.toJson(getRanking(region));
 		} else if(type == OsmAndLiveReportType.USERS_RANKING) {
 			throw new UnsupportedOperationException();
 		} else if(type == OsmAndLiveReportType.USER_RANKING) {
-			throw new UnsupportedOperationException();
-		} else if(type == OsmAndLiveReportType.RANKING) {
 			throw new UnsupportedOperationException();
 		} else if(type == OsmAndLiveReportType.RECIPIENTS) {
 			throw new UnsupportedOperationException();
@@ -211,7 +460,7 @@ public class OsmAndLiveReports {
 		}
 	}
 	
-	private Number getNumberReport(Connection conn, OsmAndLiveReportType type) throws IOException {
+	public Number getNumberReport(OsmAndLiveReportType type) throws IOException {
 		if(type == OsmAndLiveReportType.MIN_CHANGES) {
 			return getMinChanges();
 		} else if(type == OsmAndLiveReportType.REGION_RANKING_RANGE) {
@@ -221,17 +470,12 @@ public class OsmAndLiveReports {
 		} else if(type == OsmAndLiveReportType.EUR_BTC_RATE) {
 			return getEurBTCRate();
 		} else if(type == OsmAndLiveReportType.BTC_VALUE) {
-			Number eur = getNumberReport(conn, OsmAndLiveReportType.EUR_VALUE);
-			Number rt = getNumberReport(conn, OsmAndLiveReportType.EUR_BTC_RATE);
-			if (rt.doubleValue() != 0) {
-				return eur.doubleValue() / rt.doubleValue();
-			}
+			return getBtcValue();
 		} else if(type == OsmAndLiveReportType.EUR_VALUE) {
-			return getRankingRange();
+			return getEurValue();
 		} else {
 			throw new UnsupportedOperationException();
 		}
-		return 0;
 	}
 
 
@@ -240,13 +484,13 @@ public class OsmAndLiveReports {
 	}
 	
 	
-	private static class CountriesReport {
+	protected static class CountriesReport {
 		public String month;
 		public List<Country> rows = new ArrayList<Country>();
 		public Map<String, String> map = new TreeMap<String, String>();
 	}
 	
-	private static class Country {
+	protected static class Country {
 		public String map;
 		public String id;
 		public String parentid;
@@ -254,7 +498,7 @@ public class OsmAndLiveReports {
 		public String name;
 	}
 	
-	private static class SupportersReport {
+	protected static class SupportersReport {
 		public String month;
 		public Map<String, SupportersRegion> regions = new HashMap<String, SupportersRegion>();
 		public List<Supporter> rows = new ArrayList<OsmAndLiveReports.Supporter>();
@@ -262,14 +506,21 @@ public class OsmAndLiveReports {
 		public int activeCount;
 	}
 	
-	private static class SupportersRegion {
+	protected static class SupportersRegion {
 		public int count;
 		public String id;
 		public String name;
 		public float percent;
 	}
 	
-	private static class Supporter {
+	protected class TotalChangesReport {
+		public String month;
+		public String region;		
+		public int users;
+		public int changes;
+	}
+	
+	protected static class Supporter {
 		public int count;
 		public String user;
 		public String status;
@@ -279,4 +530,24 @@ public class OsmAndLiveReports {
 		
 		public float percent;
 	}
+	
+	
+	protected class RankingReport {
+		public String month;
+		public String region;		
+		public int users;
+		public int changes;
+		List<RankingRange> rows = new ArrayList<RankingRange>();
+	}
+	
+	protected class RankingRange{
+		public int minChanges;
+		public int maxChanges;
+		public int countUsers;
+		public int rank;
+		public int totalChanges;
+		public float avgChanges;
+	}
+	
+
 }
