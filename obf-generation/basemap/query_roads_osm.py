d883faf40b2d2221f9abb43d0578b255c958af2b
vshcherb <victor.shcherb@gmail.com>
2018-12-03 01:46:33 +0100

    Update query_roads_osm.py

--- obf-generation/basemap/query_roads_osm.py
+++ obf-generation/basemap/query_roads_osm.py
@@ -41,7 +41,7 @@ def process_roads(cond, filename, fields):
 		selectFields += ", " + field	
 	shift = 2
 	# roads faster but doesn't contain ferry & river
-	sql = "select osm_id, ST_AsText(ST_Transform(ST_Simplify(way,50),94326))," + \
+	sql = "select osm_id, ST_AsText(ST_Transform(ST_Simplify(way,5),94326))," + \
 	      " name, ref, tags->'int_ref' as int_ref " + selectFields + \
 	      " from planet_osm_line where " + cond + ";"
 	      # "LIMIT 1000"
