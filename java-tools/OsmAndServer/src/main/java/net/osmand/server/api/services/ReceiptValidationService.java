8a0c41559d040c7cfb338f2405f79d0729d6bcd4
crimean <alexey.kulish@osmand.net>
2018-12-29 19:12:29 +0300

    Format code

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/api/services/ReceiptValidationService.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/api/services/ReceiptValidationService.java
@@ -3,6 +3,7 @@ package net.osmand.server.api.services;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -14,11 +15,11 @@ import java.util.*;
 @Service
 public class ReceiptValidationService {
 
-    private final static String SANDBOX_URL = "https://sandbox.itunes.apple.com/verifyReceipt";
-    private final static String PRODUCTION_URL = "https://buy.itunes.apple.com/verifyReceipt";
-    private final static String BUNDLE_ID = "net.osmand.maps";
-    private final static String APPLICATION_VERSION = "1";
-    
+	private final static String SANDBOX_URL = "https://sandbox.itunes.apple.com/verifyReceipt";
+	private final static String PRODUCTION_URL = "https://buy.itunes.apple.com/verifyReceipt";
+	private final static String BUNDLE_ID = "net.osmand.maps";
+	private final static String APPLICATION_VERSION = "1";
+
 
 	public Map<String, Object> validateReceipt(final String receipt, boolean sandbox) throws Exception {
 		try {
@@ -47,62 +48,78 @@ public class ReceiptValidationService {
 		}
 	}
 
-    private HashMap<String, Object> checkValidation(JsonObject root) {
-        HashMap<String, Object> resultMap = new HashMap<>();
-        //To be determined with which field to compare
-        String productId;
-        String bundleId = root.get("receipt").getAsJsonObject().get("bundle_id").getAsString();
-        String applicationVersion = root.get("receipt").getAsJsonObject().get("application_version").getAsString();
-        JsonArray latestReceiptInfoArray = root.get("latest_receipt_info").getAsJsonArray();
+	private HashMap<String, Object> checkValidation(JsonObject root) {
+		HashMap<String, Object> resultMap = new HashMap<>();
+		//To be determined with which field to compare
+		String productId;
+		String bundleId = root.get("receipt").getAsJsonObject().get("bundle_id").getAsString();
+		String applicationVersion = root.get("receipt").getAsJsonObject().get("application_version").getAsString();
+		JsonArray latestReceiptInfoArray = root.get("latest_receipt_info").getAsJsonArray();
 
-        if(applicationVersion.equals(APPLICATION_VERSION) && bundleId.equals(BUNDLE_ID)) {
-            if(latestReceiptInfoArray.size() > 0) {
-                JsonObject latestReceipt = latestReceiptInfoArray.get(latestReceiptInfoArray.size() - 1).getAsJsonObject();
-                productId = latestReceipt.get("product_id").getAsString();
-                Long expiresDateMs = latestReceipt.get("expires_date_ms").getAsLong();
-                if(expiresDateMs > System.currentTimeMillis()) {
-                    //Subscription is valid
-                    resultMap.put("result", true);
-                    resultMap.put("message", "Subscription is valid");
-                    resultMap.put("expiration_date", expiresDateMs);
-                } else {
-                    //Subscription expired
-                    resultMap.put("result", false);
-                    resultMap.put("message", "Subscription expired");
-                }
-                return resultMap;
-            } else {
-                //No items in latest_receipt, considered valid
-                resultMap.put("result", false);
-                resultMap.put("message", "Subscription expired");
-                return  resultMap;
-            }
-        } else {
-            resultMap.put("result", false);
-            resultMap.put("message", "Inconsistency in receipt information");
-            return resultMap;
-        }
-    }
+		if (applicationVersion.equals(APPLICATION_VERSION) && bundleId.equals(BUNDLE_ID)) {
+			if (latestReceiptInfoArray.size() > 0) {
+				JsonObject latestReceipt = latestReceiptInfoArray.get(latestReceiptInfoArray.size() - 1).getAsJsonObject();
+				productId = latestReceipt.get("product_id").getAsString();
+				Long expiresDateMs = latestReceipt.get("expires_date_ms").getAsLong();
+				if (expiresDateMs > System.currentTimeMillis()) {
+					//Subscription is valid
+					resultMap.put("result", true);
+					resultMap.put("message", "Subscription is valid");
+					resultMap.put("expiration_date", expiresDateMs);
+				} else {
+					//Subscription expired
+					resultMap.put("result", false);
+					resultMap.put("message", "Subscription expired");
+				}
+				return resultMap;
+			} else {
+				//No items in latest_receipt, considered valid
+				resultMap.put("result", false);
+				resultMap.put("message", "Subscription expired");
+				return resultMap;
+			}
+		} else {
+			resultMap.put("result", false);
+			resultMap.put("message", "Inconsistency in receipt information");
+			return resultMap;
+		}
+	}
 
-    private HashMap<String, Object> mapStatus(int status) {
-        HashMap<String,Object> resultMap = new HashMap<>();
-        switch (status) {
-            case 0:
-                resultMap.put("result", true);
-                resultMap.put("message", "success");
-                return resultMap;
-            case 21000: resultMap.put("message", "App store could not read"); break;
-            case 21002: resultMap.put("message", "Data was malformed"); break;
-            case 21003: resultMap.put("message", "Receipt not authenticated"); break;
-            case 21004: resultMap.put("message", "Shared secret does not match"); break;
-            case 21005: resultMap.put("message", "Receipt server unavailable"); break;
-            case 21006: resultMap.put("message", "Receipt valid but sub expired"); break;
-            case 21007: resultMap.put("message", "Sandbox receipt sent to Production environment"); break;
-            case 21008: resultMap.put("message", "Production receipt sent to Sandbox environment"); break;
-            default:
-                resultMap.put("message", "Unknown error: status code");
-        }
-        resultMap.put("result", false);
-        return resultMap;
-    }
+	private HashMap<String, Object> mapStatus(int status) {
+		HashMap<String, Object> resultMap = new HashMap<>();
+		switch (status) {
+			case 0:
+				resultMap.put("result", true);
+				resultMap.put("message", "success");
+				return resultMap;
+			case 21000:
+				resultMap.put("message", "App store could not read");
+				break;
+			case 21002:
+				resultMap.put("message", "Data was malformed");
+				break;
+			case 21003:
+				resultMap.put("message", "Receipt not authenticated");
+				break;
+			case 21004:
+				resultMap.put("message", "Shared secret does not match");
+				break;
+			case 21005:
+				resultMap.put("message", "Receipt server unavailable");
+				break;
+			case 21006:
+				resultMap.put("message", "Receipt valid but sub expired");
+				break;
+			case 21007:
+				resultMap.put("message", "Sandbox receipt sent to Production environment");
+				break;
+			case 21008:
+				resultMap.put("message", "Production receipt sent to Sandbox environment");
+				break;
+			default:
+				resultMap.put("message", "Unknown error: status code");
+		}
+		resultMap.put("result", false);
+		return resultMap;
+	}
 }
