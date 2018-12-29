8d7c2cf963973d342a745ad0e636aa6f4ddfe068
crimean <alexey.kulish@osmand.net>
2018-12-29 19:34:56 +0300

    Return list of active inapps on validate

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/api/services/ReceiptValidationService.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/api/services/ReceiptValidationService.java
@@ -1,6 +1,7 @@
 package net.osmand.server.api.services;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -18,7 +19,7 @@ public class ReceiptValidationService {
 	private final static String SANDBOX_URL = "https://sandbox.itunes.apple.com/verifyReceipt";
 	private final static String PRODUCTION_URL = "https://buy.itunes.apple.com/verifyReceipt";
 	private final static String BUNDLE_ID = "net.osmand.maps";
-	private final static String APPLICATION_VERSION = "1";
+	//private final static String APPLICATION_VERSION = "1";
 
 
 	public Map<String, Object> validateReceipt(final String receipt, boolean sandbox) throws Exception {
@@ -51,36 +52,44 @@ public class ReceiptValidationService {
 	private HashMap<String, Object> checkValidation(JsonObject root) {
 		HashMap<String, Object> resultMap = new HashMap<>();
 		//To be determined with which field to compare
-		String productId;
 		String bundleId = root.get("receipt").getAsJsonObject().get("bundle_id").getAsString();
-		String applicationVersion = root.get("receipt").getAsJsonObject().get("application_version").getAsString();
+		//String applicationVersion = root.get("receipt").getAsJsonObject().get("application_version").getAsString();
 		JsonArray latestReceiptInfoArray = root.get("latest_receipt_info").getAsJsonArray();
 
-		if (applicationVersion.equals(APPLICATION_VERSION) && bundleId.equals(BUNDLE_ID)) {
+		if (/*applicationVersion.equals(APPLICATION_VERSION) && */bundleId.equals(BUNDLE_ID)) {
 			if (latestReceiptInfoArray.size() > 0) {
-				JsonObject latestReceipt = latestReceiptInfoArray.get(latestReceiptInfoArray.size() - 1).getAsJsonObject();
-				productId = latestReceipt.get("product_id").getAsString();
-				Long expiresDateMs = latestReceipt.get("expires_date_ms").getAsLong();
-				if (expiresDateMs > System.currentTimeMillis()) {
-					//Subscription is valid
-					resultMap.put("result", true);
-					resultMap.put("message", "Subscription is valid");
-					resultMap.put("expiration_date", expiresDateMs);
-				} else {
-					//Subscription expired
-					resultMap.put("result", false);
-					resultMap.put("message", "Subscription expired");
+				resultMap.put("result", true);
+				JsonArray inAppsArray = new JsonArray();
+				for (JsonElement jsonElement : latestReceiptInfoArray) {
+					JsonObject inAppObj = new JsonObject();
+					JsonObject receipt = jsonElement.getAsJsonObject();
+					String productId = receipt.get("product_id").getAsString();
+					inAppObj.addProperty("product_id", productId);
+					JsonElement expiresDateElement = receipt.get("expires_date_ms");
+					if (expiresDateElement != null) {
+						Long expiresDateMs = expiresDateElement.getAsLong();
+						if (expiresDateMs > System.currentTimeMillis()) {
+							//Subscription is valid
+							inAppObj.addProperty("expiration_date", expiresDateMs);
+							inAppsArray.add(inAppObj);
+						}
+					} else {
+						inAppsArray.add(inAppObj);
+					}
 				}
+				resultMap.put("products", inAppsArray);
 				return resultMap;
 			} else {
 				//No items in latest_receipt, considered valid
 				resultMap.put("result", false);
-				resultMap.put("message", "Subscription expired");
+				resultMap.put("message", "All expired");
+				resultMap.put("status", 100);
 				return resultMap;
 			}
 		} else {
 			resultMap.put("result", false);
 			resultMap.put("message", "Inconsistency in receipt information");
+			resultMap.put("status", 200);
 			return resultMap;
 		}
 	}
@@ -90,7 +99,7 @@ public class ReceiptValidationService {
 		switch (status) {
 			case 0:
 				resultMap.put("result", true);
-				resultMap.put("message", "success");
+				resultMap.put("message", "Success");
 				return resultMap;
 			case 21000:
 				resultMap.put("message", "App store could not read");
@@ -120,6 +129,7 @@ public class ReceiptValidationService {
 				resultMap.put("message", "Unknown error: status code");
 		}
 		resultMap.put("result", false);
+		resultMap.put("status", status);
 		return resultMap;
 	}
 }
