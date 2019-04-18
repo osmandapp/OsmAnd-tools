6eea787674d9d01d8d6c77f3c2365c9d0169ad5c
Victor Shcherb <victor.shcherb@gmail.com>
2019-04-18 13:46:47 +0200

    Try to fix subscription retrieval

--- java-tools/OsmAndServerUtilities/src/main/java/net/osmand/live/subscriptions/UpdateSubscription.java
+++ java-tools/OsmAndServerUtilities/src/main/java/net/osmand/live/subscriptions/UpdateSubscription.java
@@ -441,6 +441,7 @@ public class UpdateSubscription {
 		
 	    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(new FileInputStream(file)));
 	    NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+
 		// Build flow and trigger user authorization request.
 		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
 				httpTransport, jsonFactory, clientSecrets, scopes)
@@ -449,6 +450,9 @@ public class UpdateSubscription {
 						.build();
 		Builder bld = new LocalServerReceiver.Builder();
 		bld.setPort(5000);
+		if(System.getenv("HOSTNAME") != null) {
+			bld.setHost(System.getenv("HOSTNAME"));
+		}
 		Credential credential = new AuthorizationCodeInstalledApp(flow, bld.build()).authorize("user");
 		System.out.println("Credentials saved to " + dataStoreDir.getAbsolutePath());		
 		
