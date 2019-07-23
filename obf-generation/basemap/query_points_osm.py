4ddbaf17074801f1e23ad670615c8d6e0915aa71
Denis Kolomiets <madwasp79@gmail.com>
2019-07-23 13:05:44 +0300

    Change 'population' tag

--- obf-generation/basemap/query_points_osm.py
+++ obf-generation/basemap/query_points_osm.py
@@ -45,6 +45,8 @@ def process_points(cond, filename, array):
 			tag = 'tags->\'seamark:type\' as "seamark:type"'
 		if tag == 'abandoned':
 			tag = 'tags->\'abandoned\' as "abandoned"'
+		if tag == 'population':
+			tag = 'tags->\'population\' as "population"'
 		queryFields += ", " + tag
 
 	for nm in names:
