4c56bafea937f595eef47d12118135b87159c227
Denis Kolomiets <madwasp79@gmail.com>
2019-07-23 11:12:04 +0300

    Changed psql port to 5432

--- obf-generation/basemap/query_roads_osm.py
+++ obf-generation/basemap/query_roads_osm.py
@@ -20,7 +20,7 @@ def esc(s):
 
 def process_roads(cond, filename, fields):
 	print "Query %s" % cond
-	conn_string = "host='127.0.0.1' dbname='osm' user='"+os.environ['DB_USER']+"' password='"+os.environ['DB_PWD']+"' port='5433'"
+	conn_string = "host='127.0.0.1' dbname='osm' user='"+os.environ['DB_USER']+"' password='"+os.environ['DB_PWD']+"' port='5432'"
 	f = open(filename,'w')
 	f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
 	f.write('<osm version="0.6">\n')
