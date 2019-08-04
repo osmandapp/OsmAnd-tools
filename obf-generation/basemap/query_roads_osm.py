c329236cc651c1ab3d3cf07b173359a2fa8d31cc
Victor Shcherb <victor.shcherb@gmail.com>
2019-08-04 18:36:45 +0200

    Improve basemap algorithm, fix issue with duplicate id

--- obf-generation/basemap/query_roads_osm.py
+++ obf-generation/basemap/query_roads_osm.py
@@ -43,7 +43,7 @@ def process_roads(cond, filename, fields):
 	# roads faster but doesn't contain ferry & river
 	sql = "select osm_id, ST_AsText(ST_Transform(ST_Simplify(way,50,true),94326))," + \
 	      " name, ref, tags->'int_ref' as int_ref " + selectFields + \
-	      " from planet_osm_line where " + cond + " order by osmid asc;"
+	      " from planet_osm_line where " + cond + " order by osm_id asc;"
 	      # "LIMIT 1000"
 	#print sql
 	cursor.execute(sql)
