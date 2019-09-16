f39d08008fd600693e49872bab02c5ea282f75e1
Denis Kolomiets <madwasp79@gmail.com>
2019-09-16 11:56:03 +0300

    add check for admin centers for cities and towns

--- obf-generation/basemap/query_points_osm.py
+++ obf-generation/basemap/query_points_osm.py
@@ -25,13 +25,13 @@ def process_points(cond, filename, array):
 	conn_string = "host='127.0.0.1' dbname='osm' user='"+os.environ['DB_USER']+"' password='"+os.environ['DB_PWD']+"' port='5432'"
 	f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
 	f.write('<osm version="0.6">\n')
- 
 	conn = psycopg2.connect(conn_string)
 	cursor = conn.cursor()
 	shift = 2
 	queryFields = ""
 	names = ['name:af', 'name:ar', 'name:az', 'name:be', 'name:bg', 'name:bn', 'name:bpy', 'name:br', 'name:bs', 'name:ca', 'name:ceb', 'name:cs', 'name:cy', 'name:da', 'name:de', 'name:el', 'name:eo', 'name:es', 'name:et', 'name:eu', 'name:id', 'name:fa', 'name:fi', 'name:fr', 'name:fy', 'name:ga', 'name:gl', 'name:he', 'name:hi', 'name:hr', 'name:ht', 'name:hu', 'name:hy', 'name:is', 'name:it', 'name:ja', 'name:ka', 'name:kn', 'name:ko', 'name:ku', 'name:la', 'name:lb', 'name:lt', 'name:lv', 'name:mk', 'name:ml', 'name:mr', 'name:ms', 'name:nds', 'name:new', 'name:nl', 'name:nn', 'name:no', 'name:nv', 'name:os', 'name:pl', 'name:pms', 'name:pt', 'name:ro', 'name:ru', 'name:sc', 'name:sh', 'name:sk', 'name:sl', 'name:sq', 'name:sr', 'name:sv', 'name:sw', 'name:ta', 'name:te', 'name:th', 'name:tl', 'name:tr', 'name:uk', 'name:vi', 'name:vo', 'name:zh']
 	largeStatesList = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming', 'Alberta', 'British Columbia', 'Manitoba', 'New Brunswick', 'Newfoundland and Labrador', 'Northwest Territories', 'Nova Scotia', 'Nunavut', 'Ontario', 'Québec', 'Saskatchewan', 'Yukon', 'Western Australia', 'Northern Territory', 'Queensland', 'South Australia', 'New South Wales', 'Victoria', 'Tasmania']
+	adminCentersList = [line.rstrip('\n') for line in open(tools/obf-generation/basemap/admin_levels/adminCentersList.txt)]
 	for tag in array:
 		if tag == 'name:en':
 			tag = 'tags->\'name:en\' as "name:en"'
@@ -69,6 +69,7 @@ def process_points(cond, filename, array):
 	parse = re.compile('(-?[\d|\.]+)\s(-?[\d|\.]+)')
 	for row in cursor:
 		checkForLargeState = False;
+		checkForAdminCenter = False;
 		node_id = row[1] #node_id - 1
 		match = parse.search(row[0])
 		xml = '\n<node version="1" id="%s" lat="%s" lon="%s">\n' % (node_id, match.groups()[1], match.groups()[0])
@@ -79,12 +80,21 @@ def process_points(cond, filename, array):
 				value = esc(row[base])
 				if tagName == "name" and value in largeStatesList:
 					checkForLargeState = True;
+				if tagName == "name" and value in adminCentersList:
+					checkForAdminCenter = True;
 				if tagName == "place" and value == "state" and checkForLargeState:
 					xml += '\t<tag k="osmand_large_state" v="true" />\n'
 				if tagName == "place" and value == "city" :
 					pop = num(row[2], 0)
 					if pop > 500000	:
 						xml += '\t<tag k="%s" v="%s" />\n' % ("osmand_place_basemap", "city")
+				if checkForAdminCenter and tagName == "place" and ( value == "city" or value == "town"):
+					pop = num(row[2], 0)
+					if (pop > 100000):
+						pop_tag = "big"
+					else:	
+						pop_tag = "small"
+					xml += '\t<tag k="%s" v="%s" />\n' % ("osmand_admin_center", pop_tag)
 				# if tagName == 'name' and row[base+1] is not None and len(row[base+1]) > 0 :
 					# value = '' # write name:en instead of name
 				# if tagName == 'name:en' and len(value) > 0 :
@@ -100,12 +110,12 @@ def process_points(cond, filename, array):
 
 if __name__ == "__main__":
 	print "Process points"
-	process_points("place in ('continent','sea','ocean','state','country') "
+	#process_points("place in ('continent','sea','ocean','state','country') "
 				   " or \"natural\" in ('strait')", 'points_main.osm',
 				   ['name', 'name:en', 'place', 'population'])
-	#process_points("place in ('city','town') ", 'cities.osm', ['name', 'name:en', 'place', 'capital', 'population'])
+	process_points("place in ('city','town') ", 'cities.osm', ['name', 'name:en', 'place', 'capital', 'population'])
 	#process_points("place in ('city') ", 'points_only_cities.osm', ['name', 'name:en', 'place', 'capital', 'population'])
-	process_points("place in ('county') "
+	#process_points("place in ('county') "
 				   " or \"natural\" in ('peak', 'cave_entrance', 'rock', 'waterfall', 'cape', 'volcano', 'stream', 'reef')"
 				   " or tourism in ('alpine_hut') "
 				   " or tags->'seamark:type' in ('light_major') "
