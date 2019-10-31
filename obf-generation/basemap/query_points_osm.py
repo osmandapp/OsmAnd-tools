1a695e04c8271db8ee1b7d2f5820a4950f314aee
Denis Kolomiets <madwasp79@gmail.com>
2019-10-31 10:50:59 +0200

    more cities test run
    lower pop threshold from 500k to 100k

--- obf-generation/basemap/query_points_osm.py
+++ obf-generation/basemap/query_points_osm.py
@@ -75,7 +75,7 @@ def process_points(cond, filename, array):
 				value = esc(row[base])
 				if tagName == "place" and value == "city" :
 					pop = num(row[2], 0)
-					if pop > 500000	:
+					if pop > 100000	:
 						xml += '\t<tag k="%s" v="%s" />\n' % ("osmand_place_basemap", "city")
 				# if tagName == 'name' and row[base+1] is not None and len(row[base+1]) > 0 :
 					# value = '' # write name:en instead of name
@@ -92,18 +92,19 @@ def process_points(cond, filename, array):
 
 if __name__ == "__main__":
 	print "Process points"
-	process_points("place in ('continent','sea','ocean','state','country') "
-				   " or \"natural\" in ('strait')", 'points_main.osm',
-				   ['name', 'name:en', 'place', 'population'])
-	process_points("place in ('city','town') ", 'cities.osm', ['name', 'name:en', 'place', 'capital', 'population'])
+#	process_points("place in ('continent','sea','ocean','state','country') "
+#				   " or \"natural\" in ('strait')", 'points_main.osm',
+#				   ['name', 'name:en', 'place', 'population'])
+#	process_points("place in ('city','town') ", 'cities.osm', ['name', 'name:en', 'place', 'capital', 'population'])
 	process_points("place in ('city') ", 'points_only_cities.osm', ['name', 'name:en', 'place', 'capital', 'population'])
-	process_points("place in ('county') "
-				   " or \"natural\" in ('peak', 'cave_entrance', 'rock', 'waterfall', 'cape', 'volcano', 'stream', 'reef')"
-				   " or tourism in ('alpine_hut') "
-				   " or tags->'seamark:type' in ('light_major') "
-				   " or tags->'seamark:type' in ('harbour') "
-				   " or tags->'abandoned' in ('yes') "
-				   " or (tags->'population' <> '')"
-				   " or aeroway in ('aerodrome', 'airport')", 'points.osm', 
-				   ['name', 'name:en',
-				    'ref', 'ele', 'place','natural', 'seamark:type', 'abandoned', 'aeroway', 'tourism', 'iata', 'icao', 'faa', 'population'])
+#	process_points("place in ('county') "
+#				   " or \"natural\" in ('peak', 'cave_entrance', 'rock', 'waterfall', 'cape', 'volcano', 'stream', 'reef')"
+#				   " or tourism in ('alpine_hut') "
+#				   " or tags->'seamark:type' in ('light_major') "
+#				   " or tags->'seamark:type' in ('harbour') "
+#				   " or tags->'abandoned' in ('yes') "
+#				   " or (tags->'population' <> '')"
+#				   " or aeroway in ('aerodrome', 'airport')"
+#		       		   , 'points_less.osm', 
+#				   ['name', 'name:en',
+#				    'ref', 'ele', 'place','natural', 'seamark:type', 'abandoned', 'aeroway', 'tourism', 'iata', 'icao', 'faa', 'population'])
