d40ff419b5e1030fbc9aa1250a74ee81fb0b2f03
Denis Kolomiets <madwasp79@gmail.com>
2020-05-04 09:45:45 +0300

    uncomment fields

--- obf-generation/basemap/query_points_osm.py
+++ obf-generation/basemap/query_points_osm.py
@@ -92,11 +92,11 @@ def process_points(cond, filename, array):
 
 if __name__ == "__main__":
 	print "Process points"
-#	process_points("place in ('continent','sea','ocean','state','country') "
-#				   " or \"natural\" in ('strait')", 'points_main.osm',
-#				   ['name', 'name:en', 'place', 'population'])
-#	process_points("place in ('city','town') ", 'cities.osm', ['name', 'name:en', 'place', 'capital', 'population'])
-	process_points("place in ('city') ", 'points_only_cities.osm', ['name', 'name:en', 'place', 'capital', 'population'])
+	process_points("place in ('continent','sea','ocean','state','country') "
+				   " or \"natural\" in ('strait')", 'points_main.osm',
+				   ['name', 'name:en', 'place', 'population'])
+	process_points("place in ('city','town') ", 'cities.osm', ['name', 'name:en', 'place', 'capital', 'population'])
+#	process_points("place in ('city') ", 'points_only_cities.osm', ['name', 'name:en', 'place', 'capital', 'population'])
 #	process_points("place in ('county') "
 #				   " or \"natural\" in ('peak', 'cave_entrance', 'rock', 'waterfall', 'cape', 'volcano', 'stream', 'reef')"
 #				   " or tourism in ('alpine_hut') "
