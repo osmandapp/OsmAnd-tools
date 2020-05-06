e116960002123eb6790af1403a6f59a084727e84
Victor Shcherb <victor.shcherb@gmail.com>
2020-05-06 13:39:13 +0200

    Ignore backward ways for transport v2

--- java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/obf/preparation/IndexTransportCreator.java
+++ java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/obf/preparation/IndexTransportCreator.java
@@ -766,7 +766,7 @@ public class IndexTransportCreator extends AbstractIndexPartCreator {
 			if(hasRelationIncompleteWays(rel) && forwardStops.size() > 0 && 
 					directRoute.getForwardWays().size() > 1) {
 				List<Way> mergedWays = new ArrayList<>(directRoute.getForwardWays());
-				System.out.println(directRoute.getId() / 2 + " " + directRoute.getForwardWays().size()) ;
+				// System.out.println(" Process incomplete route: " + directRoute.getId() / 2 + " " + directRoute.getForwardWays().size()) ;
 				TransportRoute.mergeRouteWays(mergedWays);
 				TransportRoute.resortWaysToStopsOrder(mergedWays, forwardStops);
 				if(mergedWays.size() > 1) {
