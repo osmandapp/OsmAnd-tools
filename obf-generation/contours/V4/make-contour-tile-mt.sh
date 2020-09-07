c775dbcf1601464ac480ae57c2f57e4568add3ec
xmd5a2 <xmd5a@yandex.ru>
2020-09-07 11:20:25 +0300

    Replace pbzip2 by lbzip2 (better multithreading)

--- obf-generation/contours/V4/make-contour-tile-mt.sh
+++ obf-generation/contours/V4/make-contour-tile-mt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Multithreaded tiff tile to contour (osm) converter for OsmAnd
-# requires "parallel"
+# requires "parallel" and "lbzip2"
 # usage make-contour-tile-mt.sh input-directory output-directory number-of-threads
 
 # directory for python tools
@@ -52,7 +52,7 @@ process_tiff ()
 		if [ $? -ne 0 ]; then echo $(date)' Error creating OSM file' & exit 5;fi
 	
 		echo "Compressing to osm.bz2 …"
-		bzip2 -f $outdir$filename.osm
+		lbzip2 -f $outdir$filename.osm
 		if [ $? -ne 0 ]; then echo $(date)' Error compressing OSM file' & exit 6;fi
 		if [ -f ${TMP_DIR}$filename.shp ]; then rm ${TMP_DIR}$filename.shp ${TMP_DIR}$filename.dbf ${TMP_DIR}$filename.prj ${TMP_DIR}$filename.shx; fi
 	else echo "Skipping "$1 "(already processed)"
