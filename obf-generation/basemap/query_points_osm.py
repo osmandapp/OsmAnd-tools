cefd089f75d557ae24d552d4f1cebe051a7ca1ba
vshcherb <victor.shcherb@gmail.com>
2020-09-16 21:32:56 +0200

    Update query_points_osm.py

--- obf-generation/basemap/query_points_osm.py
+++ obf-generation/basemap/query_points_osm.py
@@ -59,7 +59,7 @@ def process_points(cond, filename, array):
 	sql = "select ST_AsText(ST_Transform(way,94326)), osm_id  " + queryFields + \
 	      " from planet_osm_point where " + cond + ";"
 	      # "LIMIT 2"
-	print sql
+	print(sql)
 	cursor.execute(sql)
  	
 	node_id =-1000
