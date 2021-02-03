3f3e94ad03bc7b56b6fe70cee2141150397e0414
xmd5a2 <xmd5a@yandex.ru>
2021-02-03 22:08:23 +0300

    Fix check-osm-bz2.sh

--- obf-generation/contours/V4/check-osm-bz2.sh
+++ obf-generation/contours/V4/check-osm-bz2.sh
@@ -14,22 +14,22 @@ if [ ! $2 ]; then
 fi
 indir=$1
 thread_number=$2
+rm -f $indir/*.osm
 
 export indir
 
-resize ()
+check ()
 {
 	filenamefull=$(basename $1)
 	filename=${filenamefull%%.*}
-	if [ ! -f $outdir/$filename.osm.bz2 ]; then
-		bzcat $indir/$filename.osm.bz2 | grep /osm > /dev/null
-		if [[ $? -eq 1 ]]; then
-			echo ERROR! $filename is incomplete. Removing.
-			rm -f $indir/$filename.osm.bz2
-		fi
-#		mv $outdir/$filename.tmp $outdir/$filename.tif
+	bzcat $indir/$filename.osm.bz2 | grep /osm > /dev/null
+	if [[ $? -eq 1 ]]; then
+		echo Error! $filename is incomplete. Removing.
+		rm -f $indir/$filename.osm.bz2
+	fi
+#	mv $outdir/$filename.tmp $outdir/$filename.tif
 	else echo "Skipping "$1 "(already processed)"
 	fi
 }
-export -f resize
-find "$indir" -maxdepth 1 -type f -name "*.osm.bz2" | sort | parallel -P $thread_number --no-notice --bar resize '{}'
+export -f check
+find "$indir" -maxdepth 1 -type f -name "*.osm.bz2" | sort | parallel -P $thread_number --no-notice --bar check '{}'
