84e53b0fe895780489c7bed60178b22219f825b6
alex-osm <alex.morozov.osm@gmail.com>
2021-08-06 11:00:17 +0300

    Remove extra logs HW

--- java-tools/OsmAndServerUtilities/src/main/java/net/osmand/live/subscriptions/HuaweiIAPHelper.java
+++ java-tools/OsmAndServerUtilities/src/main/java/net/osmand/live/subscriptions/HuaweiIAPHelper.java
@@ -187,11 +187,6 @@ public class HuaweiIAPHelper {
 			payOrderId = dataJson.optString("payOrderId", null);
 			payType = dataJson.optString("payType", null);
 			confirmed = dataJson.has("confirmed") ? dataJson.optInt("confirmed") : null;
-
-
-			System.out.println("---------- BEGIN ----------");
-			System.out.println(toString());
-			System.out.println("----------- END -----------");
 		}
 
 		@Override
