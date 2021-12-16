b3d651ad483953a0f84a7ab7f1749e0d5b34f722
Victor Shcherb <victor.shcherb@gmail.com>
2021-12-16 02:30:36 +0100

    Fix wgets

--- weather/download_noaa_gfs.sh
+++ weather/download_noaa_gfs.sh
@@ -5,23 +5,17 @@ LAYER=${LAYER:-"atmos"}
 # usually 4 hours is enough to get freshest files 
 DELAY_HOURS=${DELAY_HOURS:-4}
 BANDS=("TCDC:entire atmosphere" "TMP:2 m above ground" "PRMSL:mean sea level" "GUST:surface" "PRATE:surface")
-BANDS_DIR="cloud temperature pressure wind precip"
+BANDS_NAMES="cloud temperature pressure wind precip"
 FILE_PREFIX=${FILE_PREFIX:-"gfs.t"}
 FILE_NAME=${FILE_NAME:-"z.pgrb2.0p25.f"}
 DW_FOLDER=raw
+TIFF_FOLDER=tiff
 OS=$(uname -a)
 TIME_ZONE="GMT"
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
-
-# folder structure
-# band/YYYYmmdd/hh.tiff
-# GUST:surface/20211206/15.tiff
-
-# band/YYYYmmdd/hh/{z}/{x}/{y}.png
-# GUST:surface/20211206/15/{z}/{x}/{y}.png
 
 #https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.20211207/00/atmos/gfs.t00z.pgrb2.0p25.f000
 get_0_24() {
@@ -53,26 +47,25 @@ get_0_24() {
             filetime=$(date -d "${DATE} ${RNDHOURS}00 +${c} hours" '+%Y%m%d_%H%M')
         fi
         mkdir -p "$DW_FOLDER/"
-        ( cd $DW_FOLDER; wget -N $file_link_indx --timeout=900 )
-        ( cd $DW_FOLDER; wget -N $file_link --timeout=900 )
+        cd $DW_FOLDER; 
+        wget -N $file_link_indx --timeout=900 
+        ln -s $file_link_indx $filetime.gt.idx
+        wget -N $file_link --timeout=900 
+        ln -s $file_link $filetime.gt
+        cd ..;
     done
 }
          
 get_bands_tiff() {
-    for WFILE in ${DW_FOLDER}/*
+    for WFILE in ${DW_FOLDER}/*.gt
     do
-        if [[ $WFILE == *.idx ]]; then
-            continue
-        fi
         band_numbers=""
         for i in ${!BANDS[@]}; do
             local b_num=$(cat $WFILE.idx | grep "${BANDS[$i]}" | awk 'NR==1{print $1}' | awk -F ":" '{print $1}')
             band_numbers="$band_numbers -b $b_num"
         done
-        local b_dir=$(echo $WFILE | awk -F / '{print "/"$2"/"$3"/"}')
-        b_dir="tiff/$b_dir"
-        mkdir -p $b_dir
-        gdal_translate $band_numbers -mask "none" $WFILE ${b_dir}world.tiff
+        mkdir -p $TIFF_FOLDER/
+        gdal_translate $band_numbers -mask "none" $WFILE $TIFF_FOLDER/${WFILE}.tiff
     done
 }
 
