4687c070375684b0effea070e5e06ea8a3406b49
alisa911 <alisa.91150@gmail.com>
2022-04-27 13:04:18 +0300

    change osmc:symbol

--- java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/osm/MapRenderingTypesEncoder.java
+++ java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/osm/MapRenderingTypesEncoder.java
@@ -2,7 +2,6 @@ package net.osmand.osm;
 
 import gnu.trove.list.array.TIntArrayList;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
