301e0756349ba9a43b7e445c4243295c485ac5a5
nnngrach <nnngrach@gmail.com>
2022-09-01 22:32:10 +0300

    Update download_noaa_gfs.sh

--- weather/download_noaa_gfs.sh
+++ weather/download_noaa_gfs.sh
@@ -157,7 +157,10 @@ generate_bands_tiff() {
     done
 }
 
-get_raw_files 0 $HOURS_1H_TO_DOWNLOAD 1
+# TODO: delete
+# get_raw_files 0 $HOURS_1H_TO_DOWNLOAD 1
+
+
 
 # 1. cleanup old files to not process them
 # rm $DW_FOLDER/*.gt || true
@@ -173,7 +176,7 @@ get_raw_files 0 $HOURS_1H_TO_DOWNLOAD 1
 # get_raw_files 0 $HOURS_1H_TO_DOWNLOAD 1 & 
 # get_raw_files $HOURS_1H_TO_DOWNLOAD $HOURS_3H_TO_DOWNLOAD 3 &
 # wait
-# generate_bands_tiff
+generate_bands_tiff
 
 # find . -type f -mmin +${MINUTES_TO_KEEP} -delete
 # find . -type d -empty -delete
