30aa92e3517a5020a24d208d7bc268e9ef435bd3
nnngrach <nnngrach@gmail.com>
2022-09-13 14:42:17 +0300

    Update download_noaa_gfs.sh

--- weather/download_noaa_gfs.sh
+++ weather/download_noaa_gfs.sh
@@ -470,12 +470,12 @@ DEBUG_M0DE=1
 # ECMWD Provider
 # *************
 
-# cd "$ROOT_FOLDER/$ECMWD"
-# setup_folders_on_start
-# get_raw_ecmwf_files
-# join_tiff_files $ECMWD
-# split_tiles
-# # clean_temp_files_on_finish
+cd "$ROOT_FOLDER/$ECMWD"
+setup_folders_on_start
+get_raw_ecmwf_files
+join_tiff_files $ECMWD
+split_tiles
+# clean_temp_files_on_finish
 
 
 
@@ -486,10 +486,10 @@ DEBUG_M0DE=1
 cd "$ROOT_FOLDER/$GFS"
 setup_folders_on_start
 get_raw_gfs_files 0 $HOURS_1H_TO_DOWNLOAD 1
-# get_raw_gfs_files $HOURS_1H_TO_DOWNLOAD $HOURS_3H_TO_DOWNLOAD 3
+get_raw_gfs_files $HOURS_1H_TO_DOWNLOAD $HOURS_3H_TO_DOWNLOAD 3
 join_tiff_files $GFS
 split_tiles
-# clean_temp_files_on_finish
+clean_temp_files_on_finish
 
 
 echo "DONE!"
