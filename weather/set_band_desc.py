ff7ca1ebe09e67c354334753ff28ff2c19be0c82
Max Kojin <nnngrach@gmail.com>
2022-09-16 17:53:20 +0300

    fix weather downloading. add ecmwf provider (#510)
    * fix weather downloading. add ecmwf provider
    
    * Update download_noaa_gfs.sh
    
    * Update mktiles.sh
    
    * Update download_noaa_gfs.sh
    
    * channels renaming
    
    * Update mktiles.sh
    
    * bands renaming
    
    * Update download_noaa_gfs.sh
    
    * try/catch update
    
    * Add error handlong
    
    * Optimisation

---
+++ weather/set_band_desc.py
@@ -0,0 +1,24 @@
+import sys
+from osgeo import gdal
+
+
+def set_band_descriptions(filepath, bands):
+    """
+    filepath: path/virtual path/uri to raster
+    bands:    ((band, description), (band, description),...)
+    """
+    ds = gdal.Open(filepath, gdal.GA_Update)
+    for band, desc in bands:
+        rb = ds.GetRasterBand(band)
+        if rb is not None:
+            rb.SetDescription(desc)
+        else:
+            print("ERROR set_band_descriptions.py:  " + filepath + "  don't have band to update:  " + desc)    
+    del ds
+
+
+if __name__ == '__main__':
+    filepath = sys.argv[1]
+    bands = [int(i) for i in sys.argv[2::2]]
+    names = sys.argv[3::2]
+    set_band_descriptions(filepath, zip(bands, names))
