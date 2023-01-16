f75574b4071ddb7d4ce58ae1e5f8b94a64e07514
ivanPyrohivskyi <ivan.pirogovsky@gmail.com>
2023-01-16 20:26:10 +0200

    Read addons from rendering style folder

--- java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/NativeJavaRendering.java
+++ java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/NativeJavaRendering.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
