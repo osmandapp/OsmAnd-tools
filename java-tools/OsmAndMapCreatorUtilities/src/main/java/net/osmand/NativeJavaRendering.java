cfa1d6c3761458fdbe5eb72bf1e63b17fa25a470
ivanPyrohivskyi <ivan.pirogovsky@gmail.com>
2023-01-16 20:24:50 +0200

    Read addons from rendering style folder

--- java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/NativeJavaRendering.java
+++ java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/NativeJavaRendering.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -140,9 +141,11 @@ public class NativeJavaRendering extends NativeLibrary {
 		} else {
 			InputStream is = null;
 			InputStream is2 = null;
+			File stylesDir = null;
 			if (new File(path).exists()) {
 				is = new FileInputStream(new File(path));
 				is2 = new FileInputStream(new File(path));
+				stylesDir = new File(path).getParentFile();
 			} else {
 				is = RenderingRulesStorage.class.getResourceAsStream(path );
 				is2 = RenderingRulesStorage.class.getResourceAsStream(path);
@@ -162,6 +165,15 @@ public class NativeJavaRendering extends NativeLibrary {
 			storage.parseRulesFromXmlInputStream(is2, resolver, false);
 			is.close();
 			is2.close();
+			if (stylesDir != null) {
+				for (File file : stylesDir.listFiles()) {
+					if (file.isFile() && file.getName().endsWith("addon.render.xml")) {
+						InputStream is3 = new FileInputStream(file);
+						storage.parseRulesFromXmlInputStream(is3, resolver, true);
+						is3.close();
+					}
+				}
+			}
 		}
 		return storage;
 	}
