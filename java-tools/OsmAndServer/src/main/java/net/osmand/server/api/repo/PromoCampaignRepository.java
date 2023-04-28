92fac919158cfe52dc0c37bb2644574c5be28142
Kseniia <38911173+alisa911@users.noreply.github.com>
2023-04-28 13:37:07 +0300

    add promo (#610)
    * add promo
    
    * change comtroller, fix msg, add synchronized
    
    * add emails to table promo
    
    * refactoring
    
    * refactoring
    
    * add shema promo
    
    * refactoring promo

---
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/api/repo/PromoCampaignRepository.java
@@ -0,0 +1,45 @@
+package net.osmand.server.api.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface PromoCampaignRepository extends JpaRepository<PromoCampaignRepository.Promo, String> {
+    
+    @Entity
+    @Table(name = "promo_campaigns")
+    class Promo {
+        
+        @Id
+        @Column(name = "name")
+        public String name;
+        
+        @Column(name = "starttime")
+        @Temporal(TemporalType.TIMESTAMP)
+        public Date startTime;
+        
+        @Column(name = "endtime")
+        @Temporal(TemporalType.TIMESTAMP)
+        public Date endTime;
+        
+        @Column(name = "subactivemonths")
+        public int subActiveMonths;
+        
+        @Column(name = "numberlimit")
+        public int numberLimit;
+        
+        @Column(name = "used")
+        public int used;
+    
+        @Column(name = "lastusers")
+        public String lastUsers;
+    }
+    
+    List<PromoCampaignRepository.Promo> findAllByOrderByStartTimeDesc();
+    
+    PromoCampaignRepository.Promo findByName(String name);
+}
