b0310e4928d71c8f8b6c68719f333c4b54226fd6
alisa911 <alisa.91150@gmail.com>
2023-08-09 10:28:12 +0300

    trying increase speed list-files

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/controllers/user/MapApiController.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/controllers/user/MapApiController.java
@@ -302,8 +302,8 @@ public class MapApiController {
 		for (UserFileNoData nd : res.uniqueFiles) {
 			String ext = nd.name.substring(nd.name.lastIndexOf('.') + 1);
 			boolean isGPZTrack = nd.type.equalsIgnoreCase("gpx") && ext.equalsIgnoreCase("gpx") && !analysisPresent(ANALYSIS, nd.details);
-			boolean isFavorite = nd.type.equals(FILE_TYPE_FAVOURITES) && ext.equalsIgnoreCase("gpx") && !analysisPresent(ANALYSIS, nd.details);
-			if (isGPZTrack || isFavorite) {
+			//boolean isFavorite = nd.type.equals(FILE_TYPE_FAVOURITES) && ext.equalsIgnoreCase("gpx") && !analysisPresent(ANALYSIS, nd.details);
+			if (isGPZTrack) {
 				Optional<UserFile> of = userFilesRepository.findById(nd.id);
 				if (of.isPresent()) {
 					GPXTrackAnalysis analysis = null;
