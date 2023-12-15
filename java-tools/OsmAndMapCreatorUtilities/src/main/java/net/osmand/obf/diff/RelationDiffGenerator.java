0c5868ac6e24791166bca61b4e1112af6b7ebe35
Ivan <ivan.pirogovsky@gmail.com>
2023-12-15 17:40:02 +0200

    Live update. Fix deleted relation members processing (#730)

--- java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/obf/diff/RelationDiffGenerator.java
+++ java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/obf/diff/RelationDiffGenerator.java
@@ -11,9 +11,7 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import javax.xml.stream.XMLStreamException;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -24,35 +22,37 @@ public class RelationDiffGenerator {
         if(args.length == 1 && args[0].equals("test")) {
             args = new String[4];
             List<String> s = new ArrayList<String>();
-            s.add("/Users/macmini/OsmAnd/overpass/test3/23_04_17_20_20_before_rel.osm.gz");
-            s.add("/Users/macmini/OsmAnd/overpass/test3/23_04_17_20_20_after_rel.osm");
-            s.add("/Users/macmini/OsmAnd/overpass/test3/23_04_17_20_20_after_rel_m2.osm.gz");
-            //s.add("/Users/macmini/OsmAnd/overpass/test1/23_04_17_19_50_tmp.osm");
+            s.add("/Users/macmini/OsmAnd/overpass/issue_x/23_12_06_12_10_before_rel.osm");
+            s.add("/Users/macmini/OsmAnd/overpass/issue_x/23_12_06_12_10_after_rel.osm");
+            s.add("/Users/macmini/OsmAnd/overpass/issue_x/23_12_06_12_10_diff.osm");
+            s.add("/Users/macmini/OsmAnd/overpass/issue_x/_after_rel_m.osm");
             args = s.toArray(new String[0]);
-        } else if (args.length < 3) {
+        } else if (args.length < 4) {
             System.out.println("Usage: <path before_rel.osm.gz file> " +
                 "<path to after_rel.osm.gz file> " +
-                "<path to write result> " +
-                "<path to temporary osm file>(optional)");
+                "<path to _diff.osm file> " +
+                "<path to write result> ");
             System.exit(1);
         }
 
         String start = args[0];
         String end = args[1];
-        String result = args[2];
+        String diff = args[2];
+        String result = args[3];
 
         try {
             RelationDiffGenerator rdg = new RelationDiffGenerator();
             File startFile = new File(start);
             File endFile = new File(end);
             File targetFile = new File(result);
-            rdg.compareOsmFiles(startFile, endFile, targetFile);
+            File diffFile = new File(diff);
+            rdg.compareOsmFiles(startFile, endFile, diffFile, targetFile);
         } catch (IOException | XmlPullParserException | XMLStreamException e) {
             e.printStackTrace();
         }
     }
 
-    private void compareOsmFiles(File start, File end, File targetFile) throws IOException, XmlPullParserException, XMLStreamException {
+    private void compareOsmFiles(File start, File end, File diffFile, File targetFile) throws IOException, XmlPullParserException, XMLStreamException {
 
         long time = System.currentTimeMillis();
 
@@ -84,18 +84,30 @@ public class RelationDiffGenerator {
         Map<EntityId, Entity> startEntities = startStorage.getRegisteredEntities();
         Map<EntityId, Entity> endEntities = endStorage.getRegisteredEntities();
         int statisticAddedMembers = 0;
+
+        Set<EntityId> deletedObjIds = new HashSet<>();
+        if (diffFile != null) {
+            try {
+                DiffParser.fetchModifiedIds(diffFile, null, deletedObjIds);
+            } catch (IOException | XmlPullParserException e) {
+                e.printStackTrace();
+            }
+        }
+        long t3 = System.currentTimeMillis() - time - t2;
+        System.out.println("Parse _diff.osm in " + (t3 / 1000) + " sec.");
+
         for (Map.Entry<EntityId, Entity> e : startEntities.entrySet()) {
             Entity.EntityType entityType = e.getKey().getType();
             if (entityType != Entity.EntityType.NODE && entityType != Entity.EntityType.WAY) {
                 continue;
             }
-            if (!endEntities.containsKey(e.getKey())) {
+            if (!endEntities.containsKey(e.getKey()) && !deletedObjIds.contains(e.getKey())) {
                 endStorage.registerEntity(e.getValue(), null);
                 statisticAddedMembers++;
             }
         }
-        long t3 = System.currentTimeMillis() - time - t2;
-        System.out.println("Added " + statisticAddedMembers + " node/ways: in " + (t3 / 1000) + " sec.");
+        long t4 = System.currentTimeMillis() - time - t3;
+        System.out.println("Added " + statisticAddedMembers + " node/ways: in " + (t4 / 1000) + " sec.");
 
         File targetFileSource = targetFile.getName().endsWith(".gz") ? new File(targetFile.getAbsolutePath().replace(".gz", "")) : targetFile;
         targetFileSource.delete();
@@ -112,7 +124,7 @@ public class RelationDiffGenerator {
             gzout.close();
             targetFileSource.delete();
         }
-        long t4 = System.currentTimeMillis() - time - t3;
-        System.out.println("Wrote result into " + targetFile.getName() + " in " + (t4 / 1000)  + " sec.");
+        long t5 = System.currentTimeMillis() - time - t4;
+        System.out.println("Wrote result into " + targetFile.getName() + " in " + (t5 / 1000)  + " sec.");
     }
 }
