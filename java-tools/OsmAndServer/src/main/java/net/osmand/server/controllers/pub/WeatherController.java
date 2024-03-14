c7567fa3788489a7c2dbabcf7ec7de26b2aab67a
alisa911 <alisa.91150@gmail.com>
2024-03-14 12:02:50 +0200

    remove logs

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/controllers/pub/WeatherController.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/controllers/pub/WeatherController.java
@@ -60,11 +60,9 @@ public class WeatherController {
 	                                            @RequestParam String weatherType,
 	                                            @RequestParam(defaultValue = "false") boolean week) {
 		File folder = new File(weatherLocation + weatherType + "/tiff/");
-		LOGGER.info(String.format(folder.getAbsolutePath()));
 		List<Object[]> dt = new ArrayList<>();
 		int increment = INITIAL_INCREMENT;
 		if (folder.exists()) {
-			LOGGER.info(String.format(folder.list().length + " files"));
 			Calendar c = Calendar.getInstance();
 			c.set(Calendar.MINUTE, 0);
 			boolean isShifted = false;
@@ -76,7 +74,6 @@ public class WeatherController {
 			}
 			while (true) {
 				File fl = new File(folder, sdf.format(c.getTime()) + "00.tiff");
-				LOGGER.info(String.format(fl.getAbsolutePath()));
 				if (fl.exists()) {
 					try {
 						Object[] data = new Object[7];
