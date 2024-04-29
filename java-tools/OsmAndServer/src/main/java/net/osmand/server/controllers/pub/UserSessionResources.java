c1c51df9bc43e6d06589cc12ab0b3d772a5123ab
RZR-UA <youonly@ukr.net>
2024-04-29 08:17:49 +0200

    Revert "Fix temporary debug output"
    This reverts commit 4adaa71cc4262e41e25e8068439d64112c9aacb4.

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/controllers/pub/UserSessionResources.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/controllers/pub/UserSessionResources.java
@@ -46,9 +46,9 @@ public class UserSessionResources implements HttpSessionListener {
 	
 	public RouteCalculationProgress getRoutingProgress(HttpSession session) {
 		if (session.getAttribute(SESSION_ROUTING) instanceof RouteCalculationProgress) {
-			System.out.printf("DEBUG: cancel previous progress, last isCancelled (%d)\n",
+			System.err.printf("DEBUG: cancel previous progress, last isCancelled (%d)\n",
 					((RouteCalculationProgress) session.getAttribute(SESSION_ROUTING)).isCancelled == true ? 1 : 0);
-			System.out.printf("DEBUG: previous progress: %s\n",
+			System.err.printf("DEBUG: previous progress: %s\n",
 					((RouteCalculationProgress) session.getAttribute(SESSION_ROUTING)).getInfo(null));
 			((RouteCalculationProgress) session.getAttribute(SESSION_ROUTING)).isCancelled = true;
 		}
