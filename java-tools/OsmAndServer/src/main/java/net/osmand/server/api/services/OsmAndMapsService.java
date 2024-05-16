2509ae600071285b6c12c9503d80018f38d748c2
RZR-UA <youonly@ukr.net>
2024-05-16 12:41:59 +0200

    Revert "Use collectFinalPointsAsRoute() as approx result"
    This reverts commit c920ff2f12f835437c77c94d85c9f162fb8d6145.

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/api/services/OsmAndMapsService.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/api/services/OsmAndMapsService.java
@@ -1000,7 +1000,7 @@ public class OsmAndMapsService {
 		GpxRouteApproximation gctx = new GpxRouteApproximation(ctx);
 		List<GpxPoint> gpxPoints = router.generateGpxPoints(gctx, new LocationsHolder(waypoints));
 		GpxRouteApproximation r = router.searchGpxRoute(gctx, gpxPoints, null, useExternalTimestamps);
-		List<RouteSegmentResult> route = r.collectFinalPointsAsRoute();
+		List<RouteSegmentResult> route = r.result;
 		if (router.isUseNativeApproximation()) {
 			RouteResultPreparation preparation = new RouteResultPreparation();
 			// preparation.prepareTurnResults(gctx.ctx, route);
