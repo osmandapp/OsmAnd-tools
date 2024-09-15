0685506f7bfea6f43418e37c73de0700aec058b0
alisa911 <alisa.91150@gmail.com>
2024-09-15 17:54:17 +0300

    add wikidata filter by wlat wlon

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/api/services/WikiService.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/api/services/WikiService.java
@@ -46,8 +46,6 @@ public class WikiService {
 	private static final int LIMIT_QUERY = 1000;
 	private static final int LIMITI_QUERY = 25;
 	
-	private static final Map<Integer, Set<String>> EXCLUDED_POI_TYPES_BY_ZOOM = Map.of(14, Set.of("shop"));
-	private static final Map<Integer, Set<String>> EXCLUDED_POI_SUBTYPES_BY_ZOOM = Map.of(14, Set.of("artwork", "commercial", "memorial", "monument"));
 	
 	@Value("${osmand.wiki.location}")
 	private String pathToWikiSqlite;
@@ -93,33 +91,19 @@ public class WikiService {
 			filterParams.addAll(filters);
 		}
 		
-		Set<String> excludedPoiTypes = getExcludedTypes(EXCLUDED_POI_TYPES_BY_ZOOM, zoom);
-		Set<String> excludedPoiSubtypes = getExcludedTypes(EXCLUDED_POI_SUBTYPES_BY_ZOOM, zoom);
-		
-		String subtypeFilter = "";
-		if (!excludedPoiTypes.isEmpty()) {
-			subtypeFilter += "AND poitype NOT IN (" + excludedPoiTypes.stream().map(s -> "'" + s + "'").collect(Collectors.joining(", ")) + ") ";
-		}
-		if (!excludedPoiSubtypes.isEmpty()) {
-			subtypeFilter += "AND poisubtype NOT IN (" + excludedPoiSubtypes.stream().map(s -> "'" + s + "'").collect(Collectors.joining(", ")) + ") ";
+		String zoomCondition = "";
+		if (zoom < 13) {
+			zoomCondition = "AND wlat != 0 AND wlon != 0 ";
 		}
 		
 		String query = "SELECT id, photoId, photoTitle, catId, catTitle, depId, depTitle, wikiTitle, wikiLang, wikiDesc, wikiArticles, osmid, osmtype, poitype, poisubtype, lat, lon, wvLinks "
 				+ "FROM wikidata WHERE lat BETWEEN ? AND ? AND lon BETWEEN ? AND ? "
 				+ filterQuery
-				+ " " + subtypeFilter
+				+ zoomCondition
 				+ " ORDER BY qrank DESC LIMIT " + LIMIT_QUERY;
 		
 		return getPoiData(northWest, southEast, query, filterParams, "lat", "lon", lang);
 	}
-	
-	private static Set<String> getExcludedTypes(Map<Integer, Set<String>> map, int zoom) {
-		return map.entrySet().stream()
-				.filter(entry -> entry.getKey() >= zoom)
-				.flatMap(entry -> entry.getValue().stream())
-				.collect(Collectors.toSet());
-	}
-	
 	
 	public String getWikipediaContent(String title, String lang) {
 		String query = "SELECT hex(zipContent) AS ziphex FROM wiki.wiki_content WHERE title = ? AND lang = ?";
