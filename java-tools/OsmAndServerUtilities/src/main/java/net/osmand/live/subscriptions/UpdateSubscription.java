ea26c6e2967d74d8950f44b56241c488be2938c8
Victor Shcherb <victor.shcherb@gmail.com>
2024-10-30 13:06:34 +0200

    Validate time when check time is null

--- java-tools/OsmAndServerUtilities/src/main/java/net/osmand/live/subscriptions/UpdateSubscription.java
+++ java-tools/OsmAndServerUtilities/src/main/java/net/osmand/live/subscriptions/UpdateSubscription.java
@@ -247,7 +247,7 @@ public class UpdateSubscription {
 			if (this.subType != fromSku) {
 				continue;
 			}
-			long delayBetweenChecks = checkTime == null ? currentTime : (currentTime - checkTime.getTime());
+			long delayBetweenChecks = checkTime == null ? 0 : (currentTime - checkTime.getTime());
 			if (delayBetweenChecks < MINIMUM_WAIT_TO_REVALIDATE && !pms.verifyAll) {
 				// in case validate all (ignore minimum waiting time)
 				continue;
