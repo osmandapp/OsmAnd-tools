fc7129cdd336e1e5b849fc3c8b79c0976ffc1e09
RZR-UA <90444451+RZR-UA@users.noreply.github.com>
2025-03-12 16:57:23 +0100

    Improve POI-search-points spreading algorithm, fix OSM routes distance in distanceKmFormat (#1055)
    * Overwrite OSM routes distance (distanceKmFormat)
    
    * Introduce new POI-search-points spreading algorithm
    
    * Transfer POI-search-points to OsmGpxWriteContext
    
    * Fix warning
    
    * Use TLongHashSet for geometryBeforeCompletion
    
    * tmp filename=unique for tests
    
    * Revert "tmp filename=unique for tests"
    
    This reverts commit e13d95cdd54f304b6d9b9641bfe5c89d51bccd07.
    
    * Add route_segment_index to OSM route segments

--- java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/obf/OsmGpxWriteContext.java
+++ java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/obf/OsmGpxWriteContext.java
@@ -79,7 +79,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class OsmGpxWriteContext {
-	private final static NumberFormat distanceFormat = new DecimalFormat("0.0", new DecimalFormatSymbols(Locale.US));
+	private final static NumberFormat distanceKmFormat = new DecimalFormat("0.0", new DecimalFormatSymbols(Locale.US));
 	private final static NumberFormat latLonFormat = new DecimalFormat("0.00#####", new DecimalFormatSymbols(Locale.US));
 
 	public final QueryParams qp;
@@ -209,14 +209,36 @@ public class OsmGpxWriteContext {
 				// 1. Write points as <node> for the following <way> [MAP-section]
 				List<LatLon> pointsForPoiSearch = new ArrayList<>();
 				long idStart = baseOsmId;
-				for (WptPt p : s.getPoints()) {
-					writePoint(baseOsmId--, p, null, null, null);
-					if (pointsForPoiSearch.isEmpty() ||
-							MapUtils.getDistance(pointsForPoiSearch.get(pointsForPoiSearch.size() - 1),
-									new LatLon(p.getLatitude(), p.getLongitude())) > POI_SEARCH_POINTS_DISTANCE_M) {
-						pointsForPoiSearch.add(new LatLon(p.getLatitude(), p.getLongitude()));
+
+				List<WptPt> points = s.getPoints();
+				if (points.size() >= 2) {
+					// place the very first point in the approx middle
+					WptPt middle = points.get(points.size() / 2);
+					pointsForPoiSearch.add(new LatLon(middle.getLatitude(), middle.getLongitude()));
+
+					for (int i = 0; i < points.size(); i++) {
+						writePoint(baseOsmId--, points.get(i), null, null, null);
+
+						// place the very next points close to start/end
+						// afterward, spread points evenly along the geometry
+						int alternateIndex = i % 2 == 0 ? i : points.size() - i - 1;
+						WptPt candidate = points.get(alternateIndex);
+						WptPt firstPoint = points.get(0);
+						WptPt lastPoint = points.get(points.size() - 1);
+						double distStart = MapUtils.getDistance(candidate.getLatitude(), candidate.getLongitude(),
+								firstPoint.getLatitude(), firstPoint.getLongitude());
+						double distEnd = MapUtils.getDistance(candidate.getLatitude(), candidate.getLongitude(),
+								lastPoint.getLatitude(), lastPoint.getLongitude());
+						if (distStart > POI_SEARCH_POINTS_EDGE_DISTANCE_M && distEnd > POI_SEARCH_POINTS_EDGE_DISTANCE_M) {
+							if (pointsForPoiSearch.stream().noneMatch(wpt ->
+									MapUtils.getDistance(candidate.getLatitude(), candidate.getLongitude(),
+											wpt.getLatitude(), wpt.getLongitude()) < POI_SEARCH_POINTS_INTERVAL_M)) {
+								pointsForPoiSearch.add(new LatLon(candidate.getLatitude(), candidate.getLongitude()));
+							}
+						}
 					}
 				}
+
 				long idEnd = baseOsmId;
 
 				// 2. Write segment as <way> (without route_type tag) [MAP-section]
@@ -443,7 +465,7 @@ public class OsmGpxWriteContext {
 	}
 
 	private void addAnalysisTags(Map<String, String> gpxTrackTags, GpxTrackAnalysis analysis) {
-		gpxTrackTags.put("distance", distanceFormat.format(analysis.getTotalDistance() / 1000));
+		gpxTrackTags.put("distance", distanceKmFormat.format(analysis.getTotalDistance() / 1000));
 		if (analysis.isTimeSpecified()) {
 			gpxTrackTags.put("time_span", analysis.getTimeSpan() + "");
 			gpxTrackTags.put("time_span_no_gaps", analysis.getTimeSpanWithoutGaps() + "");
