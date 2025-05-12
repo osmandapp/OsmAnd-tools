86679cee7290552b14861152ece6d1e4721c8a33
Kseniia <38911173+alisa911@users.noreply.github.com>
2025-05-12 16:55:46 +0300

    Add order management (#1117)
    * add order-management
    
    * add register order
    
    * add order information
    
    * add WebAccessConfig
    
    * remove gihub auth
    
    * create /admin/order-mgmt
    
    ---------
    
    Co-authored-by: Kseniia Velychko <plotva@MacBook-Pro-Kseniia.local>

---
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/WebAccessConfig.java
@@ -0,0 +1,46 @@
+package net.osmand.server;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static net.osmand.server.WebSecurityConfiguration.ROLE_ADMIN;
+import static net.osmand.server.WebSecurityConfiguration.ROLE_SUPPORT;
+
+@Component
+public class WebAccessConfig {
+	private Map<String, List<String>> roles;
+
+	@Value("${osmand.web.location}")
+	private String websiteLocation;
+
+	@PostConstruct
+	public void init() throws IOException {
+		File f = new File(websiteLocation, "web-access.json");
+		ObjectMapper om = new ObjectMapper();
+		JsonNode root = om.readTree(f);
+		JsonNode r = root.get("roles");
+		roles = Map.of(
+				ROLE_ADMIN, om.convertValue(r.get("admin"), new TypeReference<>() {
+				}),
+				ROLE_SUPPORT, om.convertValue(r.get("support"), new TypeReference<>() {
+				})
+		);
+	}
+
+	public List<String> getAdmins() {
+		return roles.getOrDefault(ROLE_ADMIN, List.of());
+	}
+
+	public List<String> getSupport() {
+		return roles.getOrDefault(ROLE_SUPPORT, List.of());
+	}
+}
