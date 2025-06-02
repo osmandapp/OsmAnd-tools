6ce839f7351f511dff48d32e270dcc2e31bf2626
Kseniia Velychko <plotva@MacBook-Pro-Kseniia.local>
2025-06-02 10:57:22 +0300

    add log

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/controllers/user/AdminController.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/controllers/user/AdminController.java
@@ -1335,6 +1335,7 @@ public class AdminController {
 		public void calculateLTVValue(Map<String, TDoubleArrayList> retentionRates) {
 			priceTotalPaidEurMillis = introPriceEurMillis;
 			if (introCycles > 1) {
+				LOGGER.info("Subscription has intro cycles > 1, which is not supported for LTV calculation, sku: " + getSku());
 				throw new UnsupportedOperationException();
 			}
 			for (int i = 1; i < totalPeriods; i++) {
