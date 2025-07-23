0daf01292285f0e269f56da1d5ae543357489ff7
Grotty <gateway07@outlook.com>
2025-07-23 10:16:59 +0300

    Root dir fix

--- python/translation/translate_docs.sh
+++ python/translation/translate_docs.sh
@@ -5,11 +5,13 @@ export LANG="${1:-$LANG}"
 export INPUT_PATTERN="${2:-$INPUT_PATTERN}"
 export API_KEY="${3:-$API_KEY}"
 
+# Compute absolute path of current dir
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ -z "$WEB_DIR" ]; then
-  WEB_DIR=../web
+  WEB_DIR="$(realpath "$CURRENT_DIR/../web")"
 fi
 if [ -z "$WEB_SERVER_CONFIG_PATH" ]; then
-  export WEB_SERVER_CONFIG_PATH=../web-server-config
+  export WEB_SERVER_CONFIG_PATH="$(realpath "$CURRENT_DIR/../web-server-config")"
 fi
 
 export MODEL_TEMPERATURE=0.5
