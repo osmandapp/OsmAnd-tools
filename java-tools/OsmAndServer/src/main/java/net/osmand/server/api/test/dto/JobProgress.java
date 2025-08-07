e382fe9414afa22f225164368a689fee58e0058d
Grotty <gateway07@outlook.com>
2025-08-07 10:37:24 +0300

    Formatting fix

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/api/test/dto/JobProgress.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/api/test/dto/JobProgress.java
@@ -3,11 +3,10 @@ package net.osmand.server.api.test.dto;
 import net.osmand.server.api.test.entity.JobStatus;
 
 public record JobProgress (
-		long jobId,
-		long datasetId,
-    JobStatus status,
-    long total,
-    long processed,
-    long error,
-    long duration) {
-}
+	long jobId,
+	long datasetId,
+	JobStatus status,
+	long total,
+	long processed,
+	long error,
+	long duration) {}
