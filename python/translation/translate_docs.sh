0e525dbfd59e7ea814d52e94cf09f6f905249e97
Grotty <gateway07@outlook.com>
2025-08-26 08:15:31 +0300

    LLM_TIMEOUT fix

--- python/translation/translate_docs.sh
+++ python/translation/translate_docs.sh
@@ -16,6 +16,7 @@ if [ -z "$WEB_SERVER_CONFIG_PATH" ]; then
   export WEB_SERVER_CONFIG_PATH="$(realpath "$CURRENT_DIR/../../../web-server-config")"
 fi
 
+export LLM_TIMEOUT=60
 export MODEL_TEMPERATURE=0.0
 export MODEL=or@google/gemini-2.5-flash
 export INPUT_DIR=$WEB_DIR
