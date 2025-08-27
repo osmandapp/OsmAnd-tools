eaf9300015335c9aa66865a5a00dfbdd042a72b0
Grotty <gateway07@outlook.com>
2025-08-27 09:11:01 +0300

    MODEL_TEMPERATURE fix

--- python/translation/translate_docs.sh
+++ python/translation/translate_docs.sh
@@ -17,8 +17,8 @@ if [ -z "$WEB_SERVER_CONFIG_PATH" ]; then
 fi
 
 export LLM_TIMEOUT=360
-export MODEL_TOP_P=0.5
-export MODEL_TEMPERATURE=0.0
+export MODEL_TOP_P=1.0
+export MODEL_TEMPERATURE=0.1
 export MODEL=or@google/gemini-2.5-flash
 export INPUT_DIR=$WEB_DIR
 python3 -m python.translation.translate_docs
