7c8529fdf14639114d5de1b3fa006b62e0e1b544
vshcherb <victor.shcherb@gmail.com>
2025-10-20 00:58:47 +0300

    Add extra tag

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/api/searchtest/DataService.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/api/searchtest/DataService.java
@@ -274,8 +274,8 @@ public interface DataService extends BaseService {
 
 		List<SearchResult> searchResults = searchResult == null ? Collections.emptyList() : searchResult.results();
 		Map<String, Object> row = new LinkedHashMap<>();
-		Result firstResult = finder.findFirstResult(searchResults, targetPoint, genRow);
-		Result actualResult = finder.findActualResult(searchResults, targetPoint, datasetId, genRow);
+		Result firstResult = finder.findFirstResult(searchResults, targetPoint, row);
+		Result actualResult = finder.findActualResult(searchResults, targetPoint, datasetId, row);
 
 		int resultsCount = searchResults.size();
 		Integer distance = null, resPlace = null;
