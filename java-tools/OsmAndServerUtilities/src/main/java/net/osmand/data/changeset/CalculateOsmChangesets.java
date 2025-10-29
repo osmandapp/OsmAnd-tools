e4f12abbeae5f710a07fd133ed542ccf8a9a9bb4
ivanPyrohivskyi <ivan.pirogovsky@gmail.com>
2025-10-29 21:16:29 +0200

    Revert "Increase download changesets time from 0.5 to 1 second (#1332)"
    This reverts commit 663ba903e6e2809eb9d3fdf5d579fca40c053c27.

--- java-tools/OsmAndServerUtilities/src/main/java/net/osmand/data/changeset/CalculateOsmChangesets.java
+++ java-tools/OsmAndServerUtilities/src/main/java/net/osmand/data/changeset/CalculateOsmChangesets.java
@@ -137,7 +137,7 @@ public class CalculateOsmChangesets {
 					}
 					url += toQuery.remove(0);
 				}
-				Thread.sleep(1000);
+				Thread.sleep(500);
 				LOG.info("Query " + url);
 				HttpsURLConnection con = (HttpsURLConnection) new URL(url).openConnection();
 				con.setHostnameVerifier(new HostnameVerifier() {
