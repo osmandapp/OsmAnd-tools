e018a83a27011aac557c3aacf3059b70a18c1235
Dima-1 <softmail32@gmail.com>
2025-10-31 10:41:35 +0200

    Add labels to wiki_coords from wikidata article labels.

--- java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/wiki/wikidata/ArticleMapper.java
+++ java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/wiki/wikidata/ArticleMapper.java
@@ -13,6 +13,15 @@ import net.osmand.PlatformUtil;
 
 public class ArticleMapper implements JsonDeserializer<ArticleMapper.Article> {
 	private static final long ERROR_BATCH_SIZE = 5000L;
+	public static final String LABELS_KEY = "labels";
+	public static final String SITELINKS_KEY = "sitelinks";
+	public static final String TITLE_KEY = "title";
+	public static final String CLAIMS_KEY = "claims";
+	public static final String MAINSNAK_KEY = "mainsnak";
+	public static final String DATAVALUE_KEY = "datavalue";
+	public static final String VALUE_KEY = "value";
+	public static final String LATITUDE_KEY = "latitude";
+	public static final String LONGITUDE_KEY = "longitude";
 	private static int errorCount;
 	private static final Log log = PlatformUtil.getLog(ArticleMapper.class);
 	public static final String[] PROP_IMAGE = {"P18", "P180"};
@@ -20,33 +29,33 @@ public class ArticleMapper implements JsonDeserializer<ArticleMapper.Article> {
 	private final String PROP_COMMON_COORDS = "P625";
 
 	@Override
-    public Article deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+	public Article deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
 		Article article = new Article();
 		try {
 			JsonObject obj = (JsonObject) json;
-			Object oClaims = obj.get("claims");
+			Object oClaims = obj.get(CLAIMS_KEY);
 			if (oClaims instanceof JsonObject) {
-				JsonObject claims = obj.getAsJsonObject("claims");
+				JsonObject claims = obj.getAsJsonObject(CLAIMS_KEY);
 				JsonArray propCoords = claims.getAsJsonArray(PROP_COMMON_COORDS);
 				if (propCoords != null) {
-					JsonObject coordinatesDataValue = propCoords.get(0).getAsJsonObject().getAsJsonObject("mainsnak")
-							.getAsJsonObject("datavalue");
+					JsonObject coordinatesDataValue = propCoords.get(0).getAsJsonObject().getAsJsonObject(MAINSNAK_KEY)
+							.getAsJsonObject(DATAVALUE_KEY);
 					if (coordinatesDataValue != null) {
-						JsonObject coordinates = coordinatesDataValue.getAsJsonObject("value");
-						double lat = coordinates.getAsJsonPrimitive("latitude").getAsDouble();
-						double lon = coordinates.getAsJsonPrimitive("longitude").getAsDouble();
+						JsonObject coordinates = coordinatesDataValue.getAsJsonObject(VALUE_KEY);
+						double lat = coordinates.getAsJsonPrimitive(LATITUDE_KEY).getAsDouble();
+						double lon = coordinates.getAsJsonPrimitive(LONGITUDE_KEY).getAsDouble();
 						article.setLat(lat);
 						article.setLon(lon);
 					}
 				}
-				JsonArray propImage = null;
+				JsonArray propImage;
 				for (String property : PROP_IMAGE) {
 					propImage = claims.getAsJsonArray(property);
 					if (propImage != null) {
-						JsonObject imageDataValue = propImage.get(0).getAsJsonObject().getAsJsonObject("mainsnak")
-								.getAsJsonObject("datavalue");
+						JsonObject imageDataValue = propImage.get(0).getAsJsonObject().getAsJsonObject(MAINSNAK_KEY)
+								.getAsJsonObject(DATAVALUE_KEY);
 						if (imageDataValue != null) {
-							String image = imageDataValue.getAsJsonPrimitive("value").getAsString();
+							String image = imageDataValue.getAsJsonPrimitive(VALUE_KEY).getAsString();
 							article.setImage(image);
 							article.setImageProp(property);
 						}
@@ -55,52 +64,45 @@ public class ArticleMapper implements JsonDeserializer<ArticleMapper.Article> {
 				}
 				JsonArray propCommonCat = claims.getAsJsonArray(PROP_COMMON_CAT);
 				if (propCommonCat != null) {
-					JsonObject ccDataValue = propCommonCat.get(0).getAsJsonObject().getAsJsonObject("mainsnak")
-							.getAsJsonObject("datavalue");
+					JsonObject ccDataValue = propCommonCat.get(0).getAsJsonObject().getAsJsonObject(MAINSNAK_KEY)
+							.getAsJsonObject(DATAVALUE_KEY);
 					if (ccDataValue != null) {
-						String commonCat = ccDataValue.getAsJsonPrimitive("value").getAsString();
+						String commonCat = ccDataValue.getAsJsonPrimitive(VALUE_KEY).getAsString();
 						article.setCommonCat(commonCat);
 					}
 				}
 			}
-		Object labels = obj.get("labels");
-		if (labels != null && labels instanceof JsonObject) {
-			JsonObject labelsObj = (JsonObject) labels;
-			// Try to get English label first
-			if (labelsObj.has("en")) {
-				JsonElement enLabel = labelsObj.get("en");
-				if (enLabel.isJsonObject() && enLabel.getAsJsonObject().has("value")) {
-					article.setLabel(enLabel.getAsJsonObject().get("value").getAsString());
-				}
-			}
-			// If no English label, get first available
-			if (article.getLabel() == null && labelsObj.size() > 0) {
-				for (Map.Entry<String, JsonElement> entry : labelsObj.entrySet()) {
-					JsonElement labelEntry = entry.getValue();
-					if (labelEntry.isJsonObject() && labelEntry.getAsJsonObject().has("value")) {
-						article.setLabel(labelEntry.getAsJsonObject().get("value").getAsString());
-						break;
-					}
-				}
-			}
-		}
-		Object links = obj.get("sitelinks");
-		if (links != null) {
-			List<SiteLink> siteLinks = new ArrayList<>();
-			if (links instanceof JsonObject) {
-				for (Map.Entry<String, JsonElement> entry : ((JsonObject) links).getAsJsonObject().entrySet()) {
+			Object object = obj.get(SITELINKS_KEY);
+			if (object instanceof JsonObject links) {
+				List<SiteLink> siteLinks = new ArrayList<>();
+				for (Map.Entry<String, JsonElement> entry : links.entrySet()) {
 					String lang = entry.getKey().replace("wiki", "");
-					if (lang.equals("commons")) {
+					if ("commons".equals(lang)) {
 						continue;
 					}
-					String title = entry.getValue().getAsJsonObject().getAsJsonPrimitive("title").getAsString();
-					siteLinks.add(new SiteLink(lang, title));
+					JsonObject jsonObject = entry.getValue().getAsJsonObject();
+					JsonPrimitive title = jsonObject.getAsJsonPrimitive(TITLE_KEY);
+					if (title == null) {
+						continue;
+					}
+					siteLinks.add(new SiteLink(lang, title.getAsString()));
 				}
-			} else if (links instanceof JsonArray && ((JsonArray) links).size() > 0) {
-				throw new IllegalArgumentException();
+				article.setSiteLinks(siteLinks);
+			} else if (object instanceof JsonArray links && !links.isEmpty()) {
+				throw new IllegalArgumentException("Unexpected sitelinks array format");
 			}
-			article.setSiteLinks(siteLinks);
-		}
+
+			object = obj.get(LABELS_KEY);
+			if (object instanceof JsonObject labels) {
+				StringBuilder sb = new StringBuilder();
+				for (Map.Entry<String, JsonElement> entry : labels.entrySet()) {
+					if (!sb.isEmpty()) {
+						sb.append(",");
+					}
+					sb.append(entry.getValue().toString());
+				}
+				article.setLabels(sb.toString());
+			}
 		} catch (Exception e) {
 			errorCount++;
 			if (errorCount == ERROR_BATCH_SIZE) {
@@ -120,7 +122,7 @@ public class ArticleMapper implements JsonDeserializer<ArticleMapper.Article> {
 		private String image;
 		private String imageProp;
 		private String commonCat;
-		private String label;
+		private String labels;
 
 		public List<SiteLink> getSiteLinks() {
 			return siteLinks;
@@ -170,22 +172,15 @@ public class ArticleMapper implements JsonDeserializer<ArticleMapper.Article> {
 			this.imageProp = imageProp;
 		}
 
-		public String getLabel() {
-			return label;
+		public String getLabels() {
+			return labels;
 		}
 
-		public void setLabel(String label) {
-			this.label = label;
+		public void setLabels(String labels) {
+			this.labels = labels;
 		}
 	}
 
-	static class SiteLink {
-		String lang;
-		String title;
-
-		public SiteLink(String lang, String title) {
-			this.lang = lang;
-			this.title = title;
-		}
+	record SiteLink(String lang, String title) {
 	}
 }
