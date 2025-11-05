e15f0895db1deda0aa47a5c3566c7c5f40e1833d
Dima-1 <softmail32@gmail.com>
2025-11-05 15:37:02 +0200

    Remove fallbackTitles from osm_wikidata; in the final wiki.wikidata table, fallbackTitles comes from wiki_coords.

--- java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/wiki/WikiDatabasePreparation.java
+++ java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/wiki/WikiDatabasePreparation.java
@@ -1688,7 +1688,7 @@ public class WikiDatabasePreparation {
 		Connection commonsWikiConn = DBDialect.SQLITE.getDatabaseConnection(wikidataDB.getAbsolutePath(), log);
 		Statement st = commonsWikiConn.createStatement();
 		ResultSet rs = st
-				.executeQuery("SELECT C.id,M.lang,M.title, C.fallbackTitles FROM wiki_coords C LEFT JOIN wiki_mapping M ON M.id=C.id");
+				.executeQuery("SELECT C.id,M.lang,M.title FROM wiki_coords C LEFT JOIN wiki_mapping M ON M.id=C.id");
 		Map<Long, OsmLatLonId> res = new TreeMap<>();
 		int scan = 0;
 		long time = System.currentTimeMillis();
@@ -1696,18 +1696,17 @@ public class WikiDatabasePreparation {
 			long wid = rs.getLong(1);
 			String articleLang = rs.getString(2);
 			String articleTitle = rs.getString(3);
-			String fallbackTitles = rs.getString(4);
 			if (++scan % 500000 == 0) {
 				System.out.println("Scanning wiki to merge with OSM... " + scan + " "
 						+ (System.currentTimeMillis() - time) + " ms");
 				time = System.currentTimeMillis();
 			}
 			if (articleLang != null) {
-				setWikidataId(res, c.getCoordinates("wikipedia:" + articleLang, articleTitle), wid, fallbackTitles);
-				setWikidataId(res, c.getCoordinates("wikipedia", articleLang + ":" + articleTitle), wid, fallbackTitles);
-				setWikidataId(res, c.getCoordinates("wikipedia", articleTitle), wid, fallbackTitles);
+				setWikidataId(res, c.getCoordinates("wikipedia:" + articleLang, articleTitle), wid);
+				setWikidataId(res, c.getCoordinates("wikipedia", articleLang + ":" + articleTitle), wid);
+				setWikidataId(res, c.getCoordinates("wikipedia", articleTitle), wid);
 			}
-			setWikidataId(res, c.getCoordinates("wikidata", "Q" + wid), wid, fallbackTitles);
+			setWikidataId(res, c.getCoordinates("wikidata", "Q" + wid), wid);
 		}
 		try {
 			commonsWikiConn.createStatement().executeQuery("DROP TABLE osm_wikidata");
@@ -1717,11 +1716,10 @@ public class WikiDatabasePreparation {
 		}
 		st.execute("""
 				CREATE TABLE osm_wikidata(osmid bigint, osmtype int, wikidataid bigint, lat double, long double, \
-				tags string, poitype string, poisubtype string, wikiCommonsImg string, wikiCommonsCat string,
-				 fallbackTitles string)""");
+				tags string, poitype string, poisubtype string, wikiCommonsImg string, wikiCommonsCat string""");
 		st.close();
 		PreparedStatement ps = commonsWikiConn
-				.prepareStatement("INSERT INTO osm_wikidata VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+				.prepareStatement("INSERT INTO osm_wikidata VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 		int batch = 0;
 		for (OsmLatLonId o : res.values()) {
 			ps.setLong(1, o.id);
@@ -1734,7 +1732,6 @@ public class WikiDatabasePreparation {
 			ps.setString(8, o.amenity == null ? null : o.amenity.getSubType());
 			ps.setString(9, o.wikiCommonsImg);
 			ps.setString(10, o.wikiCommonsCat);
-			ps.setString(11, o.fallbackTitles);
 			ps.addBatch();
 			if (batch++ >= 1000) {
 				ps.executeBatch();
@@ -1783,11 +1780,10 @@ public class WikiDatabasePreparation {
 		srcConn.close();
 	}
 
-	private static void setWikidataId(Map<Long, OsmLatLonId> mp, OsmLatLonId c, long wid, String titles) {
+	private static void setWikidataId(Map<Long, OsmLatLonId> mp, OsmLatLonId c, long wid) {
 		if (c != null) {
 			c.wikidataId = wid;
-			c.fallbackTitles = titles;
-			setWikidataId(mp, c.next, wid, titles);
+			setWikidataId(mp, c.next, wid);
 			mp.put(c.type + (c.id << 2l), c);
 		}
 	}
