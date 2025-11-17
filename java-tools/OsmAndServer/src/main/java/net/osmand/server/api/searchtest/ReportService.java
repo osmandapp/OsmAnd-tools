0c33d380750b7282fc355319fef2e0fc46e8e798
Grotty <gateway07@outlook.com>
2025-11-17 11:10:47 +0300

    Errors/Not Found/Partial

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/api/searchtest/ReportService.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/api/searchtest/ReportService.java
@@ -38,6 +38,7 @@ public interface ReportService {
 			long failed,
 			long duration,
 			long found,
+			long partial,
 			long totalBytes,
 			long totalTime,
 			Map<String, Number> distanceHistogram,
@@ -218,6 +219,7 @@ public interface ReportService {
 				    count(*) FILTER (WHERE error IS NOT NULL) AS failed,
 				    sum(duration) AS duration,
 				    count(*) FILTER (WHERE COALESCE(found, res_distance <= 50)) AS found_count,
+					count(*) FILTER (WHERE Not found AND SUBSTR(COALESCE(json_extract(row, '$.actual_place'), ''), 1, INSTR(json_extract(row, '$.actual_place'), ' -') - 1) IN ('2','3','4','5')) as partial_count,
 					sum(stat_bytes) FILTER (WHERE stat_bytes IS NOT NULL) AS total_bytes,
 					sum(stat_time) FILTER (WHERE stat_time IS NOT NULL) AS total_time
 				FROM
@@ -246,6 +248,9 @@ public interface ReportService {
 			number = ((Number) result.get("found_count"));
 			long found = number == null ? 0 : number.longValue();
 
+			number = ((Number) result.get("partial_count"));
+			long partial = number == null ? 0 : number.longValue();
+
 			number = ((Number) result.get("total_bytes"));
 			long totalBytes = number == null ? 0 : number.longValue();
 
@@ -253,7 +258,7 @@ public interface ReportService {
 			long totalTime = number == null ? 0 : number.longValue();
 
 			RunStatus report = new RunStatus(Run.Status.valueOf(status), total, processed, failed,
-					duration, found, totalBytes, totalTime, null, null);
+					duration, found, partial, totalBytes, totalTime, null, null);
 			return Optional.of(report);
 		} catch (EmptyResultDataAccessException ee) {
 			LOGGER.error("Failed to process RunStatus for {}.", runId, ee);
@@ -293,10 +298,10 @@ public interface ReportService {
 		if (status == null) {
 			TestCaseStatus caseStatus = optCase.get();
 			finalStatus = new RunStatus(Run.Status.NEW, caseStatus.processed(), caseStatus.processed(),
-					caseStatus.failed(), caseStatus.duration(), 0, 0, 0, distanceHistogram, caseStatus);
+					caseStatus.failed(), caseStatus.duration(), 0, 0, 0, 0, distanceHistogram, caseStatus);
 		} else {
 			finalStatus = new RunStatus(status.status(), status.total(), status.processed(), status.failed(),
-					status.duration(), status.found(), status.totalBytes, status.totalTime, distanceHistogram, optCase.get());
+					status.duration(), status.found(), status.partial(), status.totalBytes, status.totalTime, distanceHistogram, optCase.get());
 		}
 		return Optional.of(finalStatus);
 	}
