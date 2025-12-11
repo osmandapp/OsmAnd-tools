67cabfff8b119268a1851a9b7e01d612fc308283
lamtung-monash <lam.nguyentung@monash.edu>
2025-12-11 15:42:32 +0800

    Fail with unauthorized if auth header is not provided

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/BearerTokenAuthenticationFilter.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/BearerTokenAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,8 +14,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ResponseStatusException;
 
 public class BearerTokenAuthenticationFilter extends OncePerRequestFilter {
+
+    public static final String NO_AUTHORIZATION_HEADER_WAS_PROVIDED = "No Authorization header was provided!";
 
     private UserDetailsService userDetailsService;
 
@@ -28,6 +32,11 @@ public class BearerTokenAuthenticationFilter extends OncePerRequestFilter {
 
         final String authorizationHeader = request.getHeader("Authorization");
 
+        if (authorizationHeader == null) {
+            SecurityContextHolder.clearContext();
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, NO_AUTHORIZATION_HEADER_WAS_PROVIDED);
+        }
+
         String username = null;
         String jwt = null;
 
@@ -35,7 +44,7 @@ public class BearerTokenAuthenticationFilter extends OncePerRequestFilter {
             jwt = authorizationHeader.substring(7);
             // For now, we'll just use the token as the username.
             // In a real application, you would decode the JWT and get the username from it.
-            username = jwt; 
+            username = jwt;
         }
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
