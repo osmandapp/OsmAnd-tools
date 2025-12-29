0e1094796bef054375cc8d066dc92ad0c7ed3332
vshcherb <victor.shcherb@gmail.com>
2025-12-29 20:40:12 +0200

    Clean up ws test

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/ws/WebSocketConfig.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/ws/WebSocketConfig.java
@@ -17,8 +17,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-	// FIXME origins
-    registry.addEndpoint("/osmand-websocket").setAllowedOriginPatterns("*"); 
+	// FIXME origins .setAllowedOriginPatterns("*")
+    registry.addEndpoint("/osmand-websocket"); 
   }
 
 }
