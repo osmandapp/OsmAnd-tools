e424446c1e5a69749925ce60542c49fc6f17efb0
Grotty <gateway07@outlook.com>
2026-01-18 19:02:28 +0300

    ArticleMapper fix

--- java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/wiki/wikidata/ArticleMapper.java
+++ java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/wiki/wikidata/ArticleMapper.java
@@ -3,7 +3,10 @@ package net.osmand.wiki.wikidata;
 import com.google.gson.*;
 
 import java.lang.reflect.Type;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 
