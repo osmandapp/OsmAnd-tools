0ffa505d6bcc714bf461eed4156de17db140ee93
Dima-1 <softmail32@gmail.com>
2026-01-21 21:49:38 +0200

    Remove deprecated code retrieveSourcePage with http request

--- java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/travel/WikivoyageDataGenerator.java
+++ java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/travel/WikivoyageDataGenerator.java
@@ -1,11 +1,7 @@
 package net.osmand.travel;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,7 +14,6 @@ import java.util.Map;
 
 import org.apache.commons.logging.Log;
 
-import info.bliki.wiki.filter.Encoder;
 import net.osmand.PlatformUtil;
 import net.osmand.obf.preparation.DBDialect;
 import net.osmand.util.Algorithms;
@@ -27,8 +22,6 @@ public class WikivoyageDataGenerator {
 
 	private static final Log log = PlatformUtil.getLog(WikivoyageDataGenerator.class);
 	private static final int BATCH_SIZE = 500;
-	private static final long SLEEP_WIKIMEDIA_RAW_MS = 5000;
-	
 
 	public static void main(String[] args) throws SQLException, IOException {
 		boolean uncompressed = false;
@@ -45,9 +38,8 @@ public class WikivoyageDataGenerator {
 			}
 		}
 		System.out.println("Process " + wikivoyageFile.getName() + " " + (uncompressed ? "uncompressed" : ""));
-		
-		DBDialect dialect = DBDialect.SQLITE;
-		Connection conn = (Connection) dialect.getDatabaseConnection(wikivoyageFile.getAbsolutePath(), log);
+
+		Connection conn = DBDialect.SQLITE.getDatabaseConnection(wikivoyageFile.getAbsolutePath(), log);
 		WikivoyageDataGenerator generator = new WikivoyageDataGenerator();
 		printStep("Preparing indexes");
 		conn.createStatement().execute("CREATE INDEX IF NOT EXISTS index_title_lang ON travel_articles(title,lang);");
@@ -83,10 +75,8 @@ public class WikivoyageDataGenerator {
 	}
 
 	private void updateSourceImageForArticles(Connection conn, File workingDir) throws SQLException {
-		final File imagesMetadata = new File(workingDir, "images.sqlite");
-		// delete images to fully recreate db
-		// imagesMetadata.delete();
-		Connection imagesConn = (Connection) DBDialect.SQLITE.getDatabaseConnection(imagesMetadata.getAbsolutePath(), log);
+		final File imagesMetadata = new File(workingDir, "image_sources.sqlite");
+		Connection imagesConn = DBDialect.SQLITE.getDatabaseConnection(imagesMetadata.getAbsolutePath(), log);
 		imagesConn.createStatement()
 				.execute("CREATE TABLE IF NOT EXISTS images(file text, url text, metadata text, sourcefile text)");
 		conn.createStatement().execute("DROP TABLE IF EXISTS source_image;");
@@ -94,75 +84,39 @@ public class WikivoyageDataGenerator {
 		conn.createStatement().execute("CREATE INDEX IF NOT EXISTS index_source_image ON source_image(img);");
 		
 		Map<String, String> existingImagesMapping = new LinkedHashMap<String, String>();
-		ResultSet rs1 = imagesConn.createStatement().executeQuery("SELECT file, sourcefile FROM images");
-		while (rs1.next()) {
-			String sourceFile = rs1.getString(2);
-			sourceFile = stripImageName(sourceFile);
-			existingImagesMapping.put(rs1.getString(1), sourceFile);
+		ResultSet images = imagesConn.createStatement().executeQuery("SELECT name, source FROM images");
+		while (images.next()) {
+			String sourceFile = images.getString("source");
+			existingImagesMapping.put(images.getString("name"), sourceFile);
 		}
-		rs1.close();
-		updateImagesToSource(conn, imagesConn, existingImagesMapping, "banner_title");
-//		updateImagesToSource(conn, imagesConn, existingImagesMapping, "image_title");
-		
+		images.close();
+		updateImagesToSource(conn, existingImagesMapping, "banner_title");
+//		updateImagesToSource(conn, existingImagesMapping, "image_title");
 		imagesConn.close();
-		
 	}
 
-	private void updateImagesToSource(Connection wikivoyageConn, Connection imagesConn, Map<String, String> existingImagesMapping, String imageColumn) throws SQLException {
+	private void updateImagesToSource(Connection wikivoyageConn, Map<String, String> existingImagesMapping, String imageColumn) throws SQLException {
 		try {
 			wikivoyageConn.createStatement().execute("ALTER TABLE travel_articles ADD COLUMN src_"+imageColumn);
 		} catch (Exception e) {
 			System.err.println("Column src_"+imageColumn+" already exists");
 		}
 		wikivoyageConn.createStatement().executeUpdate(String.format("UPDATE travel_articles SET src_%s = %s", imageColumn, imageColumn));
-		Map<String, String> valuesToUpdate = new LinkedHashMap<String, String>();
-//		PreparedStatement pSelect = imagesConn.prepareStatement("SELECT file, url, metadata, sourcefile FROM images WHERE file = ?");
-		//PreparedStatement pDelete = imagesConn.prepareStatement("DELETE FROM images WHERE file = ?");
-		PreparedStatement pInsert = imagesConn.prepareStatement("INSERT INTO images(file, url, metadata, sourcefile) VALUES (?, ?, ?, ?)");
 		ResultSet rs = wikivoyageConn.createStatement().executeQuery("SELECT distinct " + imageColumn
 				+ ", title, lang FROM travel_articles where " + imageColumn + " <> ''");
 		PreparedStatement pInsertSource = wikivoyageConn.prepareStatement("INSERT INTO source_image(img, source_image) VALUES(?, ?)");
-		
-		int imagesFetched = 0;
 		int imagesProcessed = 0;
 		int imagesToUpdate = 0;
 		while (rs.next()) {
 			String imageTitle = rs.getString(1);
-			String name = rs.getString(2);
-			String lang = rs.getString(3);
-			if (imageTitle == null || imageTitle.length() == 0) {
-				continue;
-			}
-			if (valuesToUpdate.containsKey(imageTitle)) {
+			if (imageTitle == null || imageTitle.isEmpty()) {
 				continue;
 			}
 			if (++imagesProcessed % 5000 == 0) {
 				System.out.println("Images metadata processed: " + imagesProcessed);
 			}
-			if (!existingImagesMapping.containsKey(imageTitle)) {
-				existingImagesMapping.put(imageTitle, null);
-				// Encoder.encodeUrl(imageTitle)
-				String originalMsg = "";
-				try {
-					String sourceFile;
-					try {
-						sourceFile = retrieveSourcePage(pInsert, "commons.wikimedia.org", imageTitle);
-					} catch (IOException e) {
-						originalMsg = e.getMessage();
-						sourceFile = retrieveSourcePage(pInsert, lang + ".wikivoyage.org", imageTitle);
-					}
-					existingImagesMapping.put(imageTitle, sourceFile);
-					if (++imagesFetched % 100 == 0) {
-						System.out.printf("Images metadata fetched: %d %s -> %s \n ", imagesFetched, imageTitle, sourceFile);
-					}
-				} catch (IOException | InterruptedException e) {
-					System.err.println("Error fetching image " + imageTitle + " " + lang + ":" + name + " "
-							+ originalMsg + " " + e.getMessage());
-				}
-			}
 			String sourceFile = existingImagesMapping.get(imageTitle);
-			valuesToUpdate.put(imageTitle, sourceFile);
-			if (sourceFile != null && trim(sourceFile).length() > 0) {
+			if (sourceFile != null && !trim(sourceFile).isEmpty()) {
 				pInsertSource.setString(1, imageTitle);
 				pInsertSource.setString(2, sourceFile);
 				pInsertSource.executeUpdate();
@@ -170,7 +124,7 @@ public class WikivoyageDataGenerator {
 			}
 		}
 		rs.close();
-		System.out.printf("Updating images %d (from %d) - fetched %d.\n", imagesToUpdate, imagesProcessed, imagesFetched);
+		System.out.printf("Updating images %d (from %d).\n", imagesToUpdate, imagesProcessed);
 		String sql = String.format("UPDATE travel_articles SET src_%s = " + 
 						" (SELECT img from source_image s where s.img = travel_articles.%s) " + 
 						" WHERE %s IN (SELECT distinct img from source_image)",
@@ -181,47 +135,6 @@ public class WikivoyageDataGenerator {
 
 	private static String trim(String s) {
 		return s.trim().replaceAll("[\\p{Cf}]", "");
-	}
-
-	private String retrieveSourcePage(PreparedStatement pInsert, String website, String imageTitle)
-			throws MalformedURLException, IOException, SQLException, InterruptedException {
-		StringBuilder metadata = new StringBuilder();
-		String param = Encoder.encodeUrl(imageTitle).replaceAll("\\(", "%28").replaceAll("\\)", "%29");
-		String metadataUrl = "https://" + website + "/w/index.php?title=File:" + param + "&action=raw";
-		URL url = new URL(metadataUrl);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-		String s;
-		String sourceFile = null;
-		while ((s = reader.readLine()) != null) {
-			if (s.contains("source=") && s.contains("File:")) {
-				sourceFile = s.substring(s.indexOf("File:") + "File:".length());
-				sourceFile = stripImageName(sourceFile);
-			}
-			metadata.append(s).append("\n");
-		}
-		pInsert.setString(1, imageTitle);
-		pInsert.setString(2, metadataUrl);
-		pInsert.setString(3, metadata.toString());
-		pInsert.setString(4, sourceFile);
-		pInsert.executeUpdate();
-		Thread.sleep(SLEEP_WIKIMEDIA_RAW_MS);
-		return sourceFile;
-	}
-
-	private String stripImageName(String sourceFile) {
-		if (sourceFile == null) {
-			return null;
-		}
-		if (sourceFile.contains("]")) {
-			sourceFile = sourceFile.substring(0, sourceFile.indexOf(']'));
-		}
-		if (sourceFile.contains("}")) {
-			sourceFile = sourceFile.substring(0, sourceFile.indexOf('}'));
-		}
-		if (sourceFile.contains("[[") || sourceFile.contains("|")) {
-			return null;
-		}
-		return sourceFile;
 	}
 
 	private void copyImagesBetweenArticles(Connection conn, String imageColumn) throws SQLException {
