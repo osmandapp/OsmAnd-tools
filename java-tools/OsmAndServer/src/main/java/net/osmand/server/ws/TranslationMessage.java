a19ed795dcfa3b4943041a748b53a2ce210e6c52
Kseniia Velychko <alisa.91150@gmail.com>
2026-06-08 15:02:36 +0300

    Add comments

--- java-tools/OsmAndServer/src/main/java/net/osmand/server/ws/TranslationMessage.java
+++ java-tools/OsmAndServer/src/main/java/net/osmand/server/ws/TranslationMessage.java
@@ -1,39 +1,38 @@
 package net.osmand.server.ws;
 
 public class TranslationMessage {
-	
+
 	public enum TranslationMessageType {
-		TEXT,
-		LOCATION,
-		JOIN,
-		LEAVE,
-		METADATA,
-		DELETE
+		TEXT,       // chat text (currently unused on the client)
+		LOCATION,   // encrypted location point
+		JOIN,       // a viewer joined: their nickname
+		LEAVE,      // a viewer left: their nickname
+		METADATA,   // sharing snapshot: who is currently sharing
+		DELETE      // translation removed by owner: its id
 	}
-	
+
 	public static final String SENDER_SYSTEM = "System";
 	public static final String SENDER_ANONYMOUS = "Anonymous";
 	public static final int SENDER_SYSTEM_ID = 0;
 	public static final int SENDER_ANONYMOUS_ID = -1;
 
-	public String sender;
-	public long sendDeviceId;
-	public long sendUserId;
-	public Object content;
+	public String sender;             // author nickname (or "System")
+	public long sendDeviceId;         // author device id (0 for system)
+	public long sendUserId;           // author user id (0 for system)
+	public Object content;            // payload, shape depends on type
 	public TranslationMessageType type;
-	public long serverReceiveTime;
-	
+	public long serverReceiveTime;    // epoch ms; used as Redis ZSet score and delta cursor
+
 	public TranslationMessage() {}
 
 	public TranslationMessage setType(TranslationMessageType type) {
 		this.type = type;
 		return this;
 	}
-	
+
 	public TranslationMessage setContent(Object content) {
 		this.content = content;
 		return this;
 	}
-	
 
 }
