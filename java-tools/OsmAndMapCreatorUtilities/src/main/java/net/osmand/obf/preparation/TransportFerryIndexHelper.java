b9ab331aea70959be31a4a3cfa5cf55bdf8081a5
Max Kojin <nnngrach@gmail.com>
2026-09-21 17:07:55 +0500

    sync code with ios

--- java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/obf/preparation/TransportFerryIndexHelper.java
+++ java-tools/OsmAndMapCreatorUtilities/src/main/java/net/osmand/obf/preparation/TransportFerryIndexHelper.java
@@ -3,6 +3,7 @@ package net.osmand.obf.preparation;
 import static net.osmand.router.TransportFerryHelper.CROSSINGS_TAG;
 import static net.osmand.router.TransportFerryHelper.FERRY_STOPS_TAG;
 import static net.osmand.router.TransportFerryHelper.JUNCTION_VALUE;
+import static net.osmand.router.TransportFerryHelper.SYNTHETIC_STOP_TAG;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -69,6 +70,7 @@ public class TransportFerryIndexHelper {
 				TransportStop stop = EntityParser.parseTransportStop(n);
 				if (!terminal) {
 					syntheticStops.put(stop.getId(), n.getId());
+					stop.setName(SYNTHETIC_STOP_TAG, "yes"); // the map hides the stop without its routes
 				}
 				directRoute.getForwardStops().add(stop);
 				backwardRoute.getForwardStops().add(0, stop);
